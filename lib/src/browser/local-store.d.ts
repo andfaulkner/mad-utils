781298392595cc9895abdcec63bd489c632d6567
andfaulkner <andfaulkner@gmail.com>
2017-05-25 08:32:21 -0400

    Transpiled local-store

--- lib/src/browser/local-store.d.ts
+++ lib/src/browser/local-store.d.ts
@@ -4,3 +4,13 @@
  * localStorage or sessionStorage. Finally, try key in 'this' binding. Return null if all fail.
  */
 export declare const getFromStorage: (key: string, store?: Object) => string;
+/**
+ * If the user is authenticated, there will be a key in localStorage.
+ * Note that this should NEVER be used to determine permissions - this
+ * is about what to display in the UI, and nothing else. The server should
+ * determine permissions - anything less is begging to be hacked.
+ *
+ * @param {string} tknKey - Key at which the auth token can be found. Defaults to 'accessToken'.
+ * @return {boolean} true if token is found at given key in browser local storage
+ */
+export declare const isAuthenticated: (tknKey?: string) => boolean;
