2ecde22ed75252ccd2f949836696c15972a638a7
andfaulkner <andfaulkner@gmail.com>
2017-05-29 04:53:24 -0400

    Added option to generate abbreviations to convertDayOfWeekNumToString

--- src/date.ts
+++ src/date.ts
@@ -25,15 +25,15 @@ export function isLeapYear(year: NumLike): boolean | never {
  * Monday is the 1st day (1 becomes 'Monday', 2 becomes 'Tuesday', 7 becomes 'Sunday')
  * Given day must be a number between 1 and 7.
  */
-export function convertDayOfWeekNumToString(day: NumRange1To7): string | never {
+export function convertDayOfWeekNumToString(day: NumRange1To7, doAbbreviate = false): string | never {
     switch(day.toString()) {
-        case '0':  return 'Sunday';
-        case '1':  return 'Monday';
-        case '2':  return 'Tuesday';
-        case '3':  return 'Wednesday';
-        case '4':  return 'Thursday';
-        case '5':  return 'Friday';
-        case '6':  return 'Saturday';
+        case '0':  return doAbbreviate ? 'Sun'   : 'Sunday';
+        case '1':  return doAbbreviate ? 'Mon'   : 'Monday';
+        case '2':  return doAbbreviate ? 'Tues'  : 'Tuesday';
+        case '3':  return doAbbreviate ? 'Wed'   : 'Wednesday';
+        case '4':  return doAbbreviate ? 'Thurs' : 'Thursday';
+        case '5':  return doAbbreviate ? 'Fri'   : 'Friday';
+        case '6':  return doAbbreviate ? 'Sat'   : 'Saturday';
         default: {
             throw new Error(`INVALID DAY OF WEEK: MUST BE NUMBER FROM 0 TO 6. Input day: ${day}`);
         }
