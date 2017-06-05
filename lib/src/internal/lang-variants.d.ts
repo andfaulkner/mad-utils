3058f2c0618ff5f2ee7a341a542300eaa75316c1
andfaulkner <andfaulkner@gmail.com>
2017-06-05 17:20:45 -0400

    Expanded language variants for english and french

---
+++ lib/src/internal/lang-variants.d.ts
@@ -0,0 +1,2 @@
+export declare const englishVariants: string[];
+export declare const frenchVariants: string[];
---
+++ src/internal/lang-variants.ts
@@ -0,0 +1,3 @@
+export const englishVariants = ['en_in', 'en', 'en_ca', 'en_us', 'en_gb', 'en_au', 'en_bz', 'en_cb',
+                                'english', 'en_ie', 'en_jm', 'en_nz', 'en_ph', 'en_za', 'en_tt'];
+export const frenchVariants = ['french', 'fr', 'fr_fr', 'fr_ca', 'fr_lu', 'fr_ch'];
