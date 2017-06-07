136ef45aa987185a62d0e8ca978faab5c5d2030e
andfaulkner <andfaulkner@gmail.com>
2017-06-07 09:49:05 -0400

    Basic 'function existence' validation unit tests

---
+++ test/src/validation.spec.ts
@@ -0,0 +1,26 @@
+/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
+
+/******************************** IMPORT ERROR MODULE FOR TESTING *********************************/
+import { expect } from 'chai';
+import { expectNonEmptyObjectExists, expectFunctionExists } from '../../src/node/test';
+
+import { m_, validation } from '../../shared';
+import { validation as validationFromNode } from '../../node';
+import { validation as validationFromBrowser } from '../../browser';
+import * as validationModule from '../../src/validation';
+
+const err = m_.validation;
+
+
+/********************************************* TESTS **********************************************/
+describe(`validation sub-module`, function() {
+    expectNonEmptyObjectExists(validation, 'validation (from shared/base export)');
+    expectNonEmptyObjectExists(m_.validation, 'validation (from m_ top-level namespace)');
+    expectNonEmptyObjectExists(validationModule, 'validation (import all from validation.ts file)');
+    expectNonEmptyObjectExists(validationFromNode, 'validation (from Node export)');
+    expectNonEmptyObjectExists(validationFromBrowser, 'validation (from Browser export)');
+
+    describe(`validation.isValidString:`, function () {
+        expectFunctionExists(validation.isValidString);
+    });
+});
