f4a71eb17bb975efb23668da588d2e796a408e16
andfaulkner <andfaulkner@gmail.com>
2017-06-08 03:48:56 -0400

    Transpiled new React export type

---
+++ lib/react.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../node_modules/@types/react/index.d.ts" />
+/// <reference types="react" />
+export declare type InputChange = React.EventHandler<React.FormEvent<HTMLInputElement>>;
+export declare type FormSubmit = React.EventHandler<React.FormEvent<HTMLInputElement>>;
