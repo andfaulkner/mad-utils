54ef32e57d4d49f353ce048c0a78343229da24d9
andfaulkner <andfaulkner@gmail.com>
2017-06-08 04:10:34 -0400

    Transpiled fixed typings on FormSubmit react type

--- lib/react.d.ts
+++ lib/react.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../node_modules/@types/react/index.d.ts" />
 /// <reference types="react" />
-export declare type InputChange = React.EventHandler<React.FormEvent<HTMLInputElement>>;
-export declare type FormSubmit = React.EventHandler<React.FormEvent<HTMLInputElement>>;
+export declare type InputChangeType = React.EventHandler<React.FormEvent<HTMLInputElement>>;
+export declare type FormSubmit = React.EventHandler<React.FormEvent<HTMLFormElement>>;
