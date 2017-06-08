09c5d8496def3ea794211b6f16e8e8c789be117e
andfaulkner <andfaulkner@gmail.com>
2017-06-08 03:46:56 -0400

    Added common react types to new react export

---
+++ react.ts
@@ -0,0 +1,4 @@
+/// <reference path="./node_modules/@types/react/index.d.ts" />
+
+export type InputChange = React.EventHandler<React.FormEvent<HTMLInputElement>>;
+export type FormSubmit = React.EventHandler<React.FormEvent<HTMLInputElement>>;
