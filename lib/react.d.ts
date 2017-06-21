f4dbdc36bc3eb3e1538ab9a01d06a616a6a65ee3
Andrew Faulkner <andfaulkner@gmail.com>
2017-06-21 06:40:15 -0400

    Transpiled React module

--- lib/react.d.ts
+++ lib/react.d.ts
@@ -1,11 +1,12 @@
 /// <reference path="../node_modules/@types/react/index.d.ts" />
 /// <reference types="react" />
+/******************************************** IMPORTS *********************************************/
 import * as React from 'react';
+/****************************************** REACT TYPES *******************************************/
 export declare type InputChange = React.EventHandler<React.FormEvent<HTMLInputElement>>;
 export declare type FormSubmit = React.EventHandler<React.FormEvent<HTMLFormElement>>;
 export { InputChange as InputChangeType };
 export { FormSubmit as FormSubmitType };
-/****************************************** REACT TYPES *******************************************/
 /**
  * Named stateless functional components / JSX elements.
  * Normally Typescript does not allow you to assign them display names, resulting in:
@@ -15,6 +16,7 @@ export { FormSubmit as FormSubmitType };
 export declare type NamedSFC<T> = ((args: T) => JSX.Element) & {
     displayName: string;
 };
+/*********************************** REACT COMPONENT FACTORIES ************************************/
 /**
  * @TODO unit test this namedStatelessComponent
  * Build a named stateless functional component.
@@ -22,21 +24,16 @@ export declare type NamedSFC<T> = ((args: T) => JSX.Element) & {
  * @param {React.StatelessComponent} sfc - Stateless functional component to name.
  * @return {React.StatelessComponent} Named stateless functional component.
  */
-export declare const namedSfcFactory: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
 export declare const buildNamedSfc: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
 export declare const buildNamedStatelessComponent: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
-export declare const namedStatelessComponentFactory: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
-export declare const namedSFCFactory: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
-export declare const buildNamedSFC: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
-export declare const nameSfc: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
-export declare const nameSFC: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
-export declare const setSFCDisplayName: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
 export declare const setSfcDisplayName: <T extends any>(displayName: string, statelessComponent: React.StatelessComponent<T>) => React.StatelessComponent<T>;
+/************************************ REACT UTILITY COMPONENTS ************************************/
 /**
  * Render the child if 'test' is truthy.
  * @param {any} test - If truthy, render children.
+ * @return {JSX.Element|null} If test is truthy, return JSX element child. Otherwise return null.
  */
-export declare const IfTrue: (props: {
+export declare const IfTruthy: (props: {
     test: any;
-    children: any;
+    children?: any;
 }) => React.ReactElement<any>;
