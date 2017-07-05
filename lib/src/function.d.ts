f569466ab7f907379f62d1b8aee8520024c41d7f
Andrew Faulkner <andfaulkner@gmail.com>
2017-07-05 04:57:56 -0400

    Added condSwitch function - to create a function-based switch statement

--- lib/src/function.d.ts
+++ lib/src/function.d.ts
@@ -1,3 +1,4 @@
+import { RealAny } from './types-iso';
 /**
  * Return a function's source code in nicely spaced array format.
  * @param {Function} func - The function to examine
@@ -8,3 +9,22 @@ export declare function getFnAsArr(func: Function): string[];
  * @alias getFnAsArr
  */
 export declare const getFunctionSrcAsArray: typeof getFnAsArr;
+/****************************************** CONDITIONALS ******************************************/
+/**
+ * Function-based switch statement. Any odd number of arguments can be given. For each pair of args,
+ * the 1st arg is a condition (which passes if truthy), and the 2nd is the value returned if the
+ * condition passes. If no conditions pass, the final arg given to the function returned. If no
+ * final arg is given, it instead throws an error.
+ *
+ * Each pair of arguments:
+ *     @param {boolean|any} cond - condition to check for truthiness
+ *     @param {any} val - value returned if the test condition is truthy.
+ * Final argument:
+ *     @param {any} defVal - value returned if no test conditions are met.
+ *
+ * @example If size is 'tiny', returns 12. If size is 'small', returns 14. Otherwise, returns 20:
+ *     condSwitch(size === 'tiny',  12,
+ *                size === 'small', 14,
+ *                                  20);
+ */
+export declare function condSwitch(cond: boolean | RealAny, val: RealAny, ...condValPairsAndOrDefVal: RealAny[]): RealAny | never;
