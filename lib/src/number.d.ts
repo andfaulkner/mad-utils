27616b51f34c175736ac5fb3be6fd421a63958bd
Andrew Faulkner <andfaulkner@gmail.com>
2017-07-05 01:59:44 -0400

    Transpiled new export from number namespace

--- lib/src/number.d.ts
+++ lib/src/number.d.ts
@@ -1,3 +1,3 @@
-export { isInt, isNumberLike } from './types-iso';
+export { isInt, isInteger, isIntegerLike, isNumberLike, isNumLike } from './types-iso';
 import * as uuid from 'uuid';
 export { uuid };
