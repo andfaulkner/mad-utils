478f26fd1160c04f98fd77571318ab6aff4f808f
Andrew Faulkner <andfaulkner@gmail.com>
2017-07-12 13:06:18 -0400

    Created minimal mock window for testing

---
+++ test/mock/mock-window.ts
@@ -0,0 +1,45 @@
+/// <reference path="../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../../node_modules/@types/node/index.d.ts" />
+
+/**************************************************************************************************
+*
+*       @file ./mock_window.ts
+*       
+*       Mocks the bare minimum amount of the window object to run the unit tests
+*
+*/
+
+import { mockBrowserStorage } from './mock-local-storage';
+
+const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 " +
+                  "(KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36";
+
+const location = {
+    href:            "http://localhost:8080/auth/en/signup/location",
+    ancestorOrigins: {},
+    origin:          "http://localhost:8080",
+    protocol:        "http:",
+    host:            "localhost:8080",
+    hostname:        "localhost",
+    port:            "8080",
+    pathname:        "/auth/en/signup/location",
+    search:          "",
+    hash:            ""
+};
+
+
+/********************************************* EXPORT *********************************************/
+/**
+ * @export
+ * Pieces of window required to run tests.
+ */
+export const window = {
+    navigator: { userAgent },
+    location,
+    sessionStorage: mockBrowserStorage(),
+    localStorage: mockBrowserStorage(),
+};
+
+
+/*************************** AUTOMATICALLY BIND WINDOW TO GLOBAL OBJECT ***************************/
+(global as any).window = window;
