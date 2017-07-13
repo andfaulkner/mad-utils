799a3e64922a301f454a73440fbd675b058fc653
Andrew Faulkner <andfaulkner@gmail.com>
2017-07-12 20:18:13 -0400

    Documented validation (in code)

--- src/validation.ts
+++ src/validation.ts
@@ -51,14 +51,12 @@ export type IsVStrOpt = {
  *     comparison: Value to run testStr against. e.g. for { type: 'min', comparison: 4, err: 'a' },
  *                 check that testStr is a number greater than or equal to 4.
  *     error: Content of 'error' displays if the conditon is not met.
- *
  * @param {Condition[]} conditions - Confirm that string meets all of these conditions.
  * @param {string} testStr - String to check the conditions against
  * @param {string} confirmStr? - [OPTIONAL] String to ensure testStr is equal to
  *                                          (if match_confirmation present)
  * @param {Function} errDisplayCb? - [OPTIONAL] Function for displaying error message.
  *                                     Defaults to alert.
- *
  * @return {boolean} true if all validation conditions are passed. False if not.
  */
 export function isValidString({conditions, testStr, confirmStr, errDisplayCb}: IsVStrOpt): boolean {
@@ -109,25 +107,48 @@ export const isEmailPotentiallyValid = (email: string): boolean => !!email.match
 export const isEmailValidBasic = isEmailPotentiallyValid;
 
 /******************************************** HELPERS *********************************************/
+
 /**
+ * Return true if there are no lowercase letters in the give string.
+ * @param {string} str - String to check for lowercase characters.
  * @return {boolean} False if string has any lowercase characters.
  */
-export function noLowercase(str: string) { return !str.match(/[a-z]/); }
+export const noLowercase = (str: string): boolean => !str.match(/[a-z]/);
+
 /**
+ * Return true if there are no uppercase letters in the give string.
+ * @param {string} str - String to check for uppercase characters.
  * @return {boolean} False if string has any uppercase characters.
  */
-export function noUppercase(str: string) { return !str.match(/[A-Z]/); }
+export const noUppercase = (str: string): boolean => !str.match(/[A-Z]/);
+
 /**
+ * Return true if there are no numbers in the give string.
  * @return {boolean} False if string has any numbers.
  */
-export function noNumber(str: string) { return !str.match(/[0-9]/); }
+export const noNumber = (str: string): boolean => !str.match(/[0-9]/);
+
+// TODO add missing special characters to noSpecialChars: , ? . { }
 /**
- * @return {boolean} False if string has any (basic) special chars. Non-exhaustive.
- * WIP - add more special chars to this.
+ * Return true if there are no special chars in the given string.
+ * Special chars: !@#$%^&*()\[\];:'"~`_\-+=|'"\/\\<>
+ * @param  {string}  str String to check for special characters
+ * @return {boolean}     True if no special chars are present in the given string
  */
-export function noSpecialChars(str: string) {
-    return !str.match(/[!@#$%^&*()\[\];:'"~`_\-+=|'"\/\\<>]/);
-}
+export const noSpecialChars = (str: string): boolean =>
+    !str.match(/[!@#$%^&*()\[\];:'"~`_\-+=|'"\/\\<>]/);
+
+/**
+ * Returns true if given string is a valid postal code e.g. M9P 8Y1. If threeCharAllowed
+ * param is set to true, also accepts the first 1/2 of a postal code e.g. M9P
+ * @param  {string}  str              check if this string is a valid postal code
+ * @param  {boolean} threeCharAllowed if true, also return true if only 3 letters long (but valid)
+ * @return {boolean}                  true if str is a valid postal code or 1st 1/2 of postal code
+ */
+const validPostalCode = (str: string, threeCharAllowed = true): boolean =>
+    threeCharAllowed
+        ? !!str.match(/^[A-Za-z][0-9][A-Za-z]\s?([0-9][A-Za-z][0-9])?$/g)
+        : !!str.match(/^[A-Za-z][0-9][A-Za-z]$/g);
 
 /**
  * Matches all characters found in English amd French, & almost all in other
