f6fd30c4b3849d7be81549aceab2b7bdbf38d73f
Andrew Faulkner <andfaulkner@gmail.com>
2017-08-31 06:45:05 -0400

    Transpiled

--- lib/react.d.ts
+++ lib/react.d.ts
@@ -2,13 +2,10 @@
 export * from './src/react/types-react';
 export * from './src/react/hocs';
 export * from './src/react/components';
-export interface FuncW2OrMoreArgs {
-    (arg1: any, arg2?: any): any;
-}
 /**
  * Log the value of a React Synthetic event's target component/element.
  * @param {string} callerName Name of function/method calling logSyntheticEvent.
  * @param {Function} logFn Function to use to perform the logging. Must accept at least 2 params.
  * @param {SyntheticEvent|any} event Event to log.
  */
-export declare const logSyntheticEventValue: (callerName: string, logFn?: FuncW2OrMoreArgs) => (event: any) => void;
+export declare const logSyntheticEventValue: (callerName: string, logFn?: (arg1: any, ...args: any[]) => any) => (event: any) => void;
