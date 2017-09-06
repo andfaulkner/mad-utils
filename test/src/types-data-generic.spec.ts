39d58dfeafe50c5747b5b302ff5c65f5bde5f50c
Andrew Faulkner <andfaulkner@gmail.com>
2017-09-06 07:53:18 -0400

    Tests for sex/gender objects

--- test/src/types-data-generic.spec.ts
+++ test/src/types-data-generic.spec.ts
@@ -20,4 +20,26 @@ describe(`Generic data tests`, function() {
             expect(canadianProvincesOrNone).to.be.frozen;
         });
     });
+
+    describe(`Sex/Gender`, function() {
+        expectNonEmptyObjectExists(sexes, 'Basic sexes object exists');
+        expectNonEmptyObjectExists(sexesWithOther, 'Basic sexes object w "other" included exists');
+        expectNonEmptyObjectExists(gender,
+            'Basic gender object (identical to sexesWithOther) exists');
+        expectNonEmptyObjectExists(genderFull,
+            'Extensive gender object (with piles of "experimental" & less common genders) exists');
+
+        it(`sexes is deep frozen`, function() {
+            expect(sexes).to.be.frozen;
+        });
+        it(`sexesWithOther is deep frozen`, function() {
+            expect(sexesWithOther).to.be.frozen;
+        });
+        it(`gender is deep frozen`, function() {
+            expect(gender).to.be.frozen;
+        });
+        it(`genderFull is deep frozen`, function() {
+            expect(genderFull).to.be.frozen;
+        });
+    });
 });
