e6aa845f2d9b098bd944d35f29cf122dd7b7e5db
Andrew Faulkner <andfaulkner@gmail.com>
2017-09-06 07:52:50 -0400

    Extended sex/gender types/data objects; included more generic data type exports; deep froze all generic data type objects

--- src/types-data-generic.ts
+++ src/types-data-generic.ts
@@ -1,5 +1,67 @@
-/********************************************** SEX ***********************************************/
-export type BiologicalSex = 'male' | 'female';
+import { deepFreeze } from './object';
+
+/***************************************** SEX AND GENDER *****************************************/
+//
+// SEX
+//
+
+export const biologicalSexes = deepFreeze({
+    male:   'male'   as 'male',
+    female: 'female' as 'female'
+});
+export { biologicalSexes as commonBiologicalSexes };
+export { biologicalSexes as sexes };
+export { biologicalSexes as commonSexes };
+
+export const biologicalSexesWithOther = deepFreeze(Object.assign({}, biologicalSexes, {
+    other: 'other' as 'other'
+}));
+export { biologicalSexesWithOther as sexesWithOther };
+export { biologicalSexesWithOther as commonSexesWithOther };
+export { biologicalSexesWithOther as commonBiologicalSexesWithOther };
+
+// Sex types
+export type BiologicalSex = keyof typeof biologicalSexes;
+export { BiologicalSex as Sex       }
+export { BiologicalSex as CommonSex }
+export { BiologicalSex as BinarySex }
+
+export type BiologicalSexWithOther = keyof typeof biologicalSexesWithOther;
+export { BiologicalSexWithOther as SexWithOther }
+export { BiologicalSexWithOther as CommonSexWithOther }
+
+
+//
+// GENDER
+//
+
+// Types, carried over from sex.
+export { BiologicalSexWithOther as Gender       }
+export { BiologicalSexWithOther as GenderCommon }
+
+// Objects, carried over form sex.
+export { biologicalSexesWithOther as gender       }
+export { biologicalSexesWithOther as genderCommon }
+
+/**
+ * Extended gender list.
+ */
+export const genderFull = deepFreeze(Object.assign({}, biologicalSexesWithOther, {
+    transMale:   'trans male'   as 'trans male',
+    transFemale: 'trans female' as 'trans female',
+    genderqueer: 'genderqueer'  as 'genderqueer',
+    genderfluid: 'genderfluid'  as 'genderfluid',
+    intersex:    'intersex'     as 'intersex',
+    agender:     'agender'      as 'agender',
+    bigender:    'bigender'     as 'bigender',
+    nonBinary:   'nonBinary'    as 'nonBinary',
+    none:        'none'         as 'none',
+}));
+
+export type GenderFull = keyof typeof genderFull;
+export { GenderFull as GenderExtensive }
+export { GenderFull as GenderExtended }
+
 
 /***************************************** PROVINCE TYPES *****************************************/
 export type CANProvince = 'Ontario'
@@ -24,33 +86,33 @@ export { CANProvince as CanadianProvince }
 export { CANProvinceOrNone as CanadianProvinceOrNone }
 
 /************************************ EXPORT PROVINCE OBJECTS *************************************/
-export type ProvinceObject = { [key: string]: CANProvince };
 export type ProvinceOrNoneObject = { [key: string]: CANProvinceOrNone };
 
 /**
  * Canadian provinces and territories list. Maps abbreviation to name.
  */
-export const CANProvinces: ProvinceObject = {
-    on: 'Ontario',
-    qc: 'Quebec',
-    nb: 'New Brunswick',
-    ns: 'Nova Scotia',
-    pe: 'Prince Edward Island',
-    mb: 'Manitoba',
-    sk: 'Saskatchewan',
-    bc: 'British Columbia',
-    nt: 'Northwest Territories',
-    nu: 'Nunavut',
-    yt: 'Yukon Territory',
-    ab: 'Alberta',
-    nl: 'Newfoundland and Labrador',
-};
+export const CANProvinces = deepFreeze({
+    on: 'Ontario'                   as 'Ontario',
+    qc: 'Quebec'                    as 'Quebec',
+    nb: 'New Brunswick'             as 'New Brunswick',
+    ns: 'Nova Scotia'               as 'Nova Scotia',
+    pe: 'Prince Edward Island'      as 'Prince Edward Island',
+    mb: 'Manitoba'                  as 'Manitoba',
+    sk: 'Saskatchewan'              as 'Saskatchewan',
+    bc: 'British Columbia'          as 'British Columbia',
+    nt: 'Northwest Territories'     as 'Northwest Territories',
+    nu: 'Nunavut'                   as 'Nunavut',
+    yt: 'Yukon Territory'           as 'Yukon Territory',
+    ab: 'Alberta'                   as 'Alberta',
+    nl: 'Newfoundland and Labrador' as 'Newfoundland and Labrador',
+});
 
 /**
  * Canadian provinces & territories list, including none as an option. Maps abbreviation to name.
  */
-export const CANProvincesOrNone: ProvinceOrNoneObject =
-    Object.assign({}, CANProvinces, { none: 'none' });
+export const CANProvincesOrNone = deepFreeze(Object.assign({}, CANProvinces, {
+    none: 'none' as 'none',
+}));
 
 // Aliases
 export { CANProvinces as canadianProvinces }
