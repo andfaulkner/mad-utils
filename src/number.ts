7cb52d24e4145621e4186f1de713ac42a9584ff3
Andrew Faulkner <andfaulkner@gmail.com>
2017-09-07 06:56:33 -0400

    Added numeric convenience types

--- src/number.ts
+++ src/number.ts
@@ -11,6 +11,16 @@ export interface UUIDNamespace {
     noDashes: () => string
 }
 
+export type Int1To10 = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+export type Int1To20 = Int1To10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+export type Int1To30 = Int1To20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30;
+export type Int1To40 = Int1To30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40;
+export type Int1To50 = Int1To40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50;
+export type Int1To60 = Int1To50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60;
+
+/** Alias for number - not a true integer. Provided to help communicate intended use of a type */
+export type Integer = number;
+
 /******************************************** EXPORTS *********************************************/
 const uuidBase = (): string => uuidImport();
 
