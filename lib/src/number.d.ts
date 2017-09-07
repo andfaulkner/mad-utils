87072b8e9dba329fb552a4d3407c3c91eb8e8445
Andrew Faulkner <andfaulkner@gmail.com>
2017-09-07 06:56:48 -0400

    Transpiled output with new numeric convenience types

--- lib/src/number.d.ts
+++ lib/src/number.d.ts
@@ -7,6 +7,14 @@ export interface UUIDNamespace {
     len6: () => string;
     noDashes: () => string;
 }
+export declare type Int1To10 = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+export declare type Int1To20 = Int1To10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20;
+export declare type Int1To30 = Int1To20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30;
+export declare type Int1To40 = Int1To30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39 | 40;
+export declare type Int1To50 = Int1To40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49 | 50;
+export declare type Int1To60 = Int1To50 | 51 | 52 | 53 | 54 | 55 | 56 | 57 | 58 | 59 | 60;
+/** Alias for number - not a true integer. Provided to help communicate intended use of a type */
+export declare type Integer = number;
 /** @return {string} Randomly generated sequence 6 characters long e.g. AB790517 */
 export declare const len6: () => string;
 /** @return {string} Randomly generated sequence 8 characters long e.g. 0E8526 */
