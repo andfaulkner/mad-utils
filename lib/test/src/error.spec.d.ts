b9550e42b0f9d4095e02629d82ef5eacf66fcc12
Andrew Faulkner <andfaulkner@gmail.com>
2017-11-07 06:29:55 -0500

    Transpiled manual type ref additions in tests

--- lib/test/src/error.spec.d.ts
+++ lib/test/src/error.spec.d.ts
@@ -1,1 +1,3 @@
 /// <reference path="../../../node_modules/@types/mocha/index.d.ts" />
+/// <reference path="../../../node_modules/@types/node/index.d.ts" />
+/// <reference path="../../../node_modules/typescript/lib/lib.es2015.d.ts" />
