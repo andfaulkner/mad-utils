5d7b81bf9b5d9ce72cde2312c06226ca87b3d62d
Andrew Faulkner <andfaulkner@gmail.com>
2017-11-28 00:19:42 -0500

    Cleaned up types.spec file

--- test/src/types.spec.ts
+++ test/src/types.spec.ts
@@ -3,17 +3,17 @@
 /// <reference path="../../node_modules/typescript/lib/lib.es2015.d.ts" />
 
 /*************************************** IMPORT TEST UTILS ****************************************/
-import { expect } from 'chai';
-import { expectFunctionExists, expectNonEmptyObjectExists  } from '../../src/node/test';
+import {expect} from 'chai';
+import {expectFunctionExists, expectNonEmptyObjectExists} from '../../src/node/test';
 import * as moment from 'moment';
 
 /******************************* IMPORT TYPES MODULES FOR TESTING *********************************/
-import { m_, types as typesIso, types, isBoolean } from '../../shared';
-import { types as typesNode } from '../../node';
-import { types as typesBrowser } from '../../browser';
+import {m_, types as typesIso, types, isBoolean} from '../../shared';
+import {types as typesNode} from '../../node';
+import {types as typesBrowser} from '../../browser';
 import * as typesModule from '../../src/types-iso';
 
-const { boolStringToBool } = typesModule;
+const {boolStringToBool} = typesModule;
 
 /********************************************* TESTS **********************************************/
 describe(`types sub-modules`, function() {
@@ -167,17 +167,19 @@ describe(`types sub-modules`, function() {
                 expect(typesIso.isIntegerLike(12.32)).to.be.false;
                 expect(typesIso.isIntegerLike(-1001.32)).to.be.false;
             });
-            it(`returns true given a string that can parse into an integer (including ` +
-                `numbers ending in a dot)`, function() {
-                expect(typesIso.isIntegerLike('0')).to.be.true;
-                expect(typesIso.isIntegerLike('1')).to.be.true;
-                expect(typesIso.isIntegerLike('-1')).to.be.true;
-                expect(typesIso.isIntegerLike('24')).to.be.true;
-                expect(typesIso.isIntegerLike('24.')).to.be.true;
-                expect(typesIso.isIntegerLike('-24.')).to.be.true;
-            });
-            it(`returns false given a string that can parse into a real number but not an integer`,
+            it(
+                `returns true given a string that can parse into an integer (including ` +
+                    `numbers ending in a dot)`,
                 function() {
+                    expect(typesIso.isIntegerLike('0')).to.be.true;
+                    expect(typesIso.isIntegerLike('1')).to.be.true;
+                    expect(typesIso.isIntegerLike('-1')).to.be.true;
+                    expect(typesIso.isIntegerLike('24')).to.be.true;
+                    expect(typesIso.isIntegerLike('24.')).to.be.true;
+                    expect(typesIso.isIntegerLike('-24.')).to.be.true;
+                },
+            );
+            it(`returns false given a string that can parse into a real number but not an integer`, function() {
                 expect(typesIso.isIntegerLike('12.32')).to.be.false;
                 expect(typesIso.isIntegerLike('-1001.32')).to.be.false;
                 expect(typesIso.isIntegerLike('.32')).to.be.false;
@@ -233,12 +235,14 @@ describe(`types sub-modules`, function() {
                 expect(typesIso.isNonexistentOrString({})).to.equal(false);
                 expect(typesIso.isNonexistentOrString([])).to.equal(false);
                 expect(typesIso.isNonexistentOrString(['asdf'])).to.equal(false);
-                expect(typesIso.isNonexistentOrString({ 'asdf': 'asdf' })).to.equal(false);
+                expect(typesIso.isNonexistentOrString({asdf: 'asdf'})).to.equal(false);
                 expect(typesIso.isNonexistentOrString(0)).to.equal(false);
                 expect(typesIso.isNonexistentOrString(1)).to.equal(false);
                 expect(typesIso.isNonexistentOrString(NaN)).to.equal(false);
                 expect(typesIso.isNonexistentOrString(Object)).to.equal(false);
-                expect(typesIso.isNonexistentOrString(typesIso.isNonexistentOrString)).to.equal(false);
+                expect(typesIso.isNonexistentOrString(typesIso.isNonexistentOrString)).to.equal(
+                    false,
+                );
             });
         });
 
@@ -250,7 +254,7 @@ describe(`types sub-modules`, function() {
                 expect(typesIso.isArray(['asdf', '123', null])).to.be.true;
                 expect(typesIso.isArray([null])).to.be.true;
                 expect(typesIso.isArray(new Array())).to.be.true;
-                class ArrayExtender extends Array {};
+                class ArrayExtender extends Array {}
                 expect(typesIso.isArray(new ArrayExtender())).to.be.true;
             });
             it(`returns false for non-arrays`, function() {
@@ -258,7 +262,7 @@ describe(`types sub-modules`, function() {
                 expect(typesIso.isArray(0)).to.be.false;
                 expect(typesIso.isArray(1)).to.be.false;
                 expect(typesIso.isArray({})).to.be.false;
-                expect(typesIso.isArray({ asdf: [] })).to.be.false;
+                expect(typesIso.isArray({asdf: []})).to.be.false;
                 expect(typesIso.isArray('123123')).to.be.false;
                 expect(typesIso.isArray('[]')).to.be.false;
                 expect(typesIso.isArray('sasffwe')).to.be.false;
@@ -275,8 +279,7 @@ describe(`types sub-modules`, function() {
 
         describe(`isTrue function`, function() {
             expectFunctionExists(typesIso.isTrue);
-            it(`returns true if given true, 'true', 'True', or 'TRUE', regardless of arg 2's value`,
-            function() {
+            it(`returns true if given true, 'true', 'True', or 'TRUE', regardless of arg 2's value`, function() {
                 expect(typesIso.isTrue(true)).to.be.true;
                 expect(typesIso.isTrue('true')).to.be.true;
                 expect(typesIso.isTrue('TRUE')).to.be.true;
@@ -290,8 +293,7 @@ describe(`types sub-modules`, function() {
                 expect(typesIso.isTrue('TRUE', false)).to.be.true;
                 expect(typesIso.isTrue('True', false)).to.be.true;
             });
-            it(`if arg 2 false, returns false for all vals except true, 'true', 'True', or 'TRUE'`,
-            function() {
+            it(`if arg 2 false, returns false for all vals except true, 'true', 'True', or 'TRUE'`, function() {
                 expect(typesIso.isTrue(null)).to.be.false;
                 expect(typesIso.isTrue(false)).to.be.false;
                 expect(typesIso.isTrue('TrUe')).to.be.false;
@@ -302,7 +304,7 @@ describe(`types sub-modules`, function() {
                 expect(typesIso.isTrue({})).to.be.false;
                 expect(typesIso.isTrue([])).to.be.false;
                 expect(typesIso.isTrue([true, true])).to.be.false;
-                expect(typesIso.isTrue({ true: true })).to.be.false;
+                expect(typesIso.isTrue({true: true})).to.be.false;
                 expect(typesIso.isTrue(1)).to.be.false;
                 expect(typesIso.isTrue(0)).to.be.false;
                 expect(typesIso.isTrue(123456)).to.be.false;
@@ -349,7 +351,7 @@ describe(`types sub-modules`, function() {
                 expect(typesIso.isFalse({})).to.be.false;
                 expect(typesIso.isFalse([])).to.be.false;
                 expect(typesIso.isFalse([false, false])).to.be.false;
-                expect(typesIso.isFalse({ false: false })).to.be.false;
+                expect(typesIso.isFalse({false: false})).to.be.false;
                 expect(typesIso.isFalse(1)).to.be.false;
                 expect(typesIso.isFalse(0)).to.be.false;
                 expect(typesIso.isFalse(123456)).to.be.false;
@@ -388,29 +390,31 @@ describe(`types sub-modules`, function() {
                 expect(typesIso.isDateLike('0')).to.be.true;
                 expect(typesIso.isDateLike({})).to.be.true;
                 expect(typesIso.isDateLike([])).to.be.true;
-                expect(typesIso.isDateLike({ months: 10 })).to.be.true;
-                expect(typesIso.isDateLike({ month: 10 })).to.be.true;
-                expect(typesIso.isDateLike({ years: 10 })).to.be.true;
-                expect(typesIso.isDateLike({ year: 2015 })).to.be.true;
-                expect(typesIso.isDateLike({ date: 24 })).to.be.true;
-                expect(typesIso.isDateLike({ dates: 22 })).to.be.true;
-                expect(typesIso.isDateLike({ day: 21 })).to.be.true;
-                expect(typesIso.isDateLike({ days: 20 })).to.be.true;
-                expect(typesIso.isDateLike({ millisecond: 900 })).to.be.true;
-                expect(typesIso.isDateLike({ seconds: 56 })).to.be.true;
+                expect(typesIso.isDateLike({months: 10})).to.be.true;
+                expect(typesIso.isDateLike({month: 10})).to.be.true;
+                expect(typesIso.isDateLike({years: 10})).to.be.true;
+                expect(typesIso.isDateLike({year: 2015})).to.be.true;
+                expect(typesIso.isDateLike({date: 24})).to.be.true;
+                expect(typesIso.isDateLike({dates: 22})).to.be.true;
+                expect(typesIso.isDateLike({day: 21})).to.be.true;
+                expect(typesIso.isDateLike({days: 20})).to.be.true;
+                expect(typesIso.isDateLike({millisecond: 900})).to.be.true;
+                expect(typesIso.isDateLike({seconds: 56})).to.be.true;
             });
-            it(`should return false for negative numbers, anything moment considers invalid, & ` +
-               `nonsensical objects`, function()
-            {
-                expect(typesIso.isDateLike('-1234')).to.be.false;
-                expect(typesIso.isDateLike(-1234)).to.be.false;
-                expect(typesIso.isDateLike(NaN)).to.be.false;
-                expect(typesIso.isDateLike(['asdf'])).to.be.false;
-                expect(typesIso.isDateLike([''])).to.be.false;
-                expect(typesIso.isDateLike({ oko: 123 })).to.be.false;
-                expect(typesIso.isDateLike({ oko: 123, date: 10 })).to.be.false;
-                expect(typesIso.isDateLike('12312354')).to.be.false;
-            });
+            it(
+                `should return false for negative numbers, anything moment considers invalid, & ` +
+                    `nonsensical objects`,
+                function() {
+                    expect(typesIso.isDateLike('-1234')).to.be.false;
+                    expect(typesIso.isDateLike(-1234)).to.be.false;
+                    expect(typesIso.isDateLike(NaN)).to.be.false;
+                    expect(typesIso.isDateLike(['asdf'])).to.be.false;
+                    expect(typesIso.isDateLike([''])).to.be.false;
+                    expect(typesIso.isDateLike({oko: 123})).to.be.false;
+                    expect(typesIso.isDateLike({oko: 123, date: 10})).to.be.false;
+                    expect(typesIso.isDateLike('12312354')).to.be.false;
+                },
+            );
         });
 
         describe(`isMultilangTextObj`, function() {
@@ -423,7 +427,7 @@ describe(`types sub-modules`, function() {
             class SingletonObject {
                 someString: string;
                 constructor(someString) {
-                    this.someString = someString
+                    this.someString = someString;
                 }
             }
 
@@ -431,7 +435,7 @@ describe(`types sub-modules`, function() {
             class NonSingletonObject {
                 someString: string;
                 constructor(someString) {
-                    this.someString = someString
+                    this.someString = someString;
                 }
             }
             // Instance of singleton class.
@@ -449,14 +453,16 @@ describe(`types sub-modules`, function() {
                 expect(singletonObject).to.be.instanceof(SingletonObject);
                 expect(singletonObject.someString).to.eql('my string argument');
             });
-            it(`ensures re-instantiating a class it's applied to for a second time results in ` +
-                `returning the original object`, function()
-            {
-                const newInstance = new SingletonObject('another string');
-                expect(newInstance.someString).to.eql('my string argument');
-                expect(newInstance.someString).to.eql(singletonObject.someString);
-                expect(newInstance).to.eql(singletonObject);
-            });
+            it(
+                `ensures re-instantiating a class it's applied to for a second time results in ` +
+                    `returning the original object`,
+                function() {
+                    const newInstance = new SingletonObject('another string');
+                    expect(newInstance.someString).to.eql('my string argument');
+                    expect(newInstance.someString).to.eql(singletonObject.someString);
+                    expect(newInstance).to.eql(singletonObject);
+                },
+            );
             it(`has no effect on classes it's not applied to`, function() {
                 const nonSingletonInstance = new NonSingletonObject('string one');
                 const nonSingletonInstance2 = new NonSingletonObject('string two');
@@ -464,7 +470,6 @@ describe(`types sub-modules`, function() {
                 expect(nonSingletonInstance2.someString).to.eql('string two');
             });
         });
-
     });
 
     describe(`isNumber`, function() {
@@ -474,7 +479,7 @@ describe(`types sub-modules`, function() {
             Reflect.defineProperty(NewNumber.prototype, 'subtract', {
                 value: function subtract(numToSubtract: number | Number): Number {
                     return new Number(Number(this) - (numToSubtract as any));
-                }
+                },
             });
         });
 
@@ -502,7 +507,7 @@ describe(`types sub-modules`, function() {
             expect(typesIso.isNumber('asdf')).to.equal(false);
             expect(typesIso.isNumber('123')).to.equal(false);
             expect(typesIso.isNumber({})).to.equal(false);
-            expect(typesIso.isNumber({ a: 'eh' })).to.equal(false);
+            expect(typesIso.isNumber({a: 'eh'})).to.equal(false);
             expect(typesIso.isNumber([])).to.equal(false);
             expect(typesIso.isNumber([1, 2, 3])).to.equal(false);
             expect(typesIso.isNumber(false)).to.equal(false);
@@ -557,9 +562,9 @@ describe(`types sub-modules`, function() {
             expect(() => (boolStringToBool as any)(() => 'value')).to.throw();
             expect(() => (boolStringToBool as any)('BAD_STRING')).to.throw();
             expect(() => (boolStringToBool as any)('')).to.throw();
-            expect(() => (boolStringToBool as any)(({ false: false }))).to.throw();
+            expect(() => (boolStringToBool as any)({false: false})).to.throw();
             expect(() => (boolStringToBool as any)([])).to.throw();
-            expect(() => (boolStringToBool as any)([{ false: false }])).to.throw();
+            expect(() => (boolStringToBool as any)([{false: false}])).to.throw();
             expect(() => (boolStringToBool as any)(null)).to.throw();
             expect(() => (boolStringToBool as any)(undefined)).to.throw();
             expect(() => (boolStringToBool as any)(123)).to.throw();
@@ -572,9 +577,9 @@ describe(`types sub-modules`, function() {
             expect(() => (boolStringToBool as any)(() => 'value', false)).to.not.throw();
             expect(() => (boolStringToBool as any)('BAD_STRING', false)).to.not.throw();
             expect(() => (boolStringToBool as any)('', false)).to.not.throw();
-            expect(() => (boolStringToBool as any)(({ false: false }), false)).to.not.throw();
+            expect(() => (boolStringToBool as any)({false: false}, false)).to.not.throw();
             expect(() => (boolStringToBool as any)([], false)).to.not.throw();
-            expect(() => (boolStringToBool as any)([{ false: false }], false)).to.not.throw();
+            expect(() => (boolStringToBool as any)([{false: false}], false)).to.not.throw();
             expect(() => (boolStringToBool as any)(null, false)).to.not.throw();
             expect(() => (boolStringToBool as any)(undefined, false)).to.not.throw();
             expect(() => (boolStringToBool as any)(123, false)).to.not.throw();
@@ -587,9 +592,9 @@ describe(`types sub-modules`, function() {
             expect(() => (boolStringToBool as any)(() => 'value', false)).to.not.throw();
             expect(() => (boolStringToBool as any)('BAD_STRING', false)).to.not.throw();
             expect(() => (boolStringToBool as any)('', false)).to.not.throw();
-            expect(() => (boolStringToBool as any)(({ false: false }), false)).to.not.throw();
+            expect(() => (boolStringToBool as any)({false: false}, false)).to.not.throw();
             expect(() => (boolStringToBool as any)([], false)).to.not.throw();
-            expect(() => (boolStringToBool as any)([{ false: false }], false)).to.not.throw();
+            expect(() => (boolStringToBool as any)([{false: false}], false)).to.not.throw();
             expect(() => (boolStringToBool as any)(null, false)).to.not.throw();
             expect(() => (boolStringToBool as any)(undefined, false)).to.not.throw();
             expect(() => (boolStringToBool as any)(123, false)).to.not.throw();
