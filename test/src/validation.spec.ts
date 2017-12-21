80a3fc59d1b45f2f21f5872ff02a7c34d43992d1
Andrew Faulkner <andfaulkner@gmail.com>
2017-12-20 19:15:06 -0500

    Unit tested isValidString (validation module)

--- test/src/validation.spec.ts
+++ test/src/validation.spec.ts
@@ -4,7 +4,7 @@
 import { expect } from 'chai';
 import { expectNonEmptyObjectExists, expectFunctionExists } from '../../src/node/test';
 
-import { m_, validation } from '../../shared';
+import { m_, validation, isValidString } from '../../shared';
 import { validation as validationFromNode } from '../../node';
 import { validation as validationFromBrowser } from '../../browser';
 import * as validationModule from '../../src/validation';
@@ -21,6 +21,42 @@ describe(`validation sub-module`, function() {
     expectNonEmptyObjectExists(validationFromBrowser, 'validation (from Browser export)');
 
     describe(`validation.isValidString:`, function () {
-        expectFunctionExists(validation.isValidString);
+        expectFunctionExists(isValidString);
+        it(`should not throw if given a valid string`, function() {
+            expect(isValidStringTestConditions('okok')).to.be.true;
+            expect(isValidStringTestConditions('ooo')).to.be.true;
+            expect(isValidStringTestConditions('oooooooooo')).to.be.true;
+        });
+        it(`should return false if given a string missing a required value`, function() {
+            expect(isValidStringTestConditions('asdfasdf'), `At least one 'o' is required`)
+                .to.be.false;
+        });
+        it(`should return false if given a string that's too short`, function() {
+            expect(isValidStringTestConditions('o'), `String is too short`).to.be.false;
+        });
+
+        it(`should return false if given a string that's too long`, function() {
+            expect(isValidStringTestConditions('giorshgiuehgega'), `String is too long`).to.be.false;
+        });
+
+        it(`should return false if given a string with a disallowed match`, function() {
+            expect(isValidStringTestConditions(' oofewof '), `No whitespace allowed`).to.be.false;
+        });
     });
 });
+
+/**
+ *
+ */
+function isValidStringTestConditions(testStr: string, confirmStr?: string): boolean | never {
+    return isValidString({
+        testStr,
+        conditions: [
+            {type: 'match',    matcher: /o/g, errMsg: `text.input_doesnt_match`},
+            {type: 'no_match', matcher: / /g, errMsg: `text.no_whitespace`     },
+            {type: 'max',      matcher: 10,   errMsg: `text.input_too_long`    },
+            {type: 'min',      matcher: 3,    errMsg: `text.input_too_short`   },
+        ],
+        errDisplayCb: (err: Error) => console.log(err)
+    });
+}
