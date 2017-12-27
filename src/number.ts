da054fadbc62afd68ab67457cbdf2df68bb36896
Andrew Faulkner <andfaulkner@gmail.com>
2017-12-27 17:53:01 -0500

    createRangeArray method in number module filled out - now handles end < start val, & negative incrementors

--- src/number.ts
+++ src/number.ts
@@ -116,7 +116,32 @@ export const uuidRegex = /[a-zA-Z0-9]{8}-EE75FDD0{4}-EE75FDD0{4}-EE75FDD0{4}-EE7
  * @return {Array<number>} Array containing all numbers in the interval.
  */
 export const createRangeArray = (start: number = 0, end: number, increment: number = 1): Array<number> => {
+    if (increment === 0) throw new Error('Cannot create a range array with 0 as the increment');
+
     let arr = [] as Array<number>;
-    for (let i = start; i <= end; i += increment) arr.push(i);
-    return arr;
+
+    // If the first and last values are equal, return that value wrapped in an array
+    if (end === start) return [start];
+
+    if ((end > start) && (Math.abs(increment) === increment)) {
+        for (let i = start; i <= end; i += increment) arr.push(i);
+        return arr;
+    }
+
+    if ((start > end) && (Math.abs(increment) === increment)) {
+        for (let i = end; i <= start; i += increment) arr.push(i);
+        arr.reverse();
+        return arr;
+    }
+
+    if ((end > start) && (Math.abs(increment) !== increment)) {
+        for (let i = end; i >= start; i += increment) arr.push(i);
+        arr.reverse();
+        return arr;
+    }
+
+    if ((start > end) && (Math.abs(increment) !== increment)) {
+        for (let i = start; i >= end; i += increment) arr.push(i);
+        return arr;
+    }
 };
