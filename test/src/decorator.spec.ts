552565578c726b2da40badc4a99c818e673b214d
Andrew Faulkner <andfaulkner@gmail.com>
2018-02-07 09:33:31 -0500

    Added minor test for new getDecoratorType function (needs more testing)

--- test/src/decorator.spec.ts
+++ test/src/decorator.spec.ts
@@ -4,13 +4,14 @@
 
 /****************************** IMPORT DECORATOR MODULE FOR TESTING *******************************/
 import {expect} from 'chai';
-import {expectNonEmptyObjectExists} from '../../src/node/test';
+import {expectNonEmptyObjectExists, expectFunctionExists} from '../../src/node/test';
 
 import {m_, decorator} from '../../shared';
 
 import {decorator as decoratorFromNode} from '../../node';
 import {decorator as decoratorFromBrowser} from '../../browser';
 import * as decoratorModule from '../../src/decorator';
+import {getDecoratorType} from '../../src/decorator';
 
 const dec = m_.decorator;
 
@@ -21,4 +22,14 @@ describe(`decorator sub-module`, function() {
     expectNonEmptyObjectExists(decoratorModule, 'decorator (import all from decorator.ts file)');
     expectNonEmptyObjectExists(decoratorFromNode, 'decorator (from Node export)');
     expectNonEmptyObjectExists(decoratorFromBrowser, 'decorator (from Browser export)');
+
+    expectFunctionExists(getDecoratorType, `getDecoratorType function in decorator module`);
+
+    describe(`getDecoratorType`, function() {
+        it(`returns CLASS if given a function as a single argument`, function() {
+            expect(getDecoratorType(() => '')).to.eql('CLASS');
+        });
+
+        console.log(`\n\n\n*** decorator.spec :: MORE getDecoratorType TESTS REQUIRED ***\n\n\n`);
+    });
 });
