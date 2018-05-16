18c43bd272b1e0d509b952a1f9a7ce620ab914c8
Andrew Faulkner <andfaulkner@gmail.com>
2018-05-16 04:31:33 -0400

    Add throttle function

--- src/function.ts
+++ src/function.ts
@@ -51,7 +51,7 @@ export const loop5 = <T>(func: ((...args) => T)): T[] => loopN(5, func);
  */
 export function delegateAll<S, I>(self: S, input: I): S & I {
     Object.keys(input).forEach(k =>
-        Object.defineProperty(self, k, Object.getOwnPropertyDescriptor(input, k)),
+        Object.defineProperty(self, k, Object.getOwnPropertyDescriptor(input, k))
     );
     return self as S & I;
 }
@@ -113,9 +113,68 @@ export function condSwitch(
         throw new Error(
             'No matching val found. To avoid throwing in this scenario, pass args to consSwitch' +
                 'in pairs, where #1 is the test, and #2 is the return val if test is truthy - then ' +
-                'follow them with a final "else" value to return if no other tests are truthy',
+                'follow them with a final "else" value to return if no other tests are truthy'
         );
     }
 
     return condValPairsAndOrDefVal[0];
+}
+
+/************************************* RUN TIMING / LIMITING **************************************/
+/**
+ * @param {Function} cb Call max 1X/[wait]ms & call at interval start if [immediate]=true {default}
+ * @param {number} wait Time to wait before next call of function allowed
+ * @param {boolean} immediate If true, call at the beginning of the interval {default=true}
+ */
+function throttle(cb: (() => any), wait: number, immediate?: boolean): (() => void);
+function throttle<A = any>(
+    cb: ((a: A) => any),
+    wait: number,
+    immediate?: boolean
+): ((a: A) => void);
+function throttle<A = any, B = any>(
+    cb: (a: A, b: B) => any,
+    wait: number,
+    immediate?: boolean
+): (a: A, b: B) => void;
+function throttle<A = any, B = any, C = any>(
+    cb: (a: A, b: B, c: C) => any,
+    wait: number,
+    immediate?: boolean
+): (a: A, b: B, c: C) => void;
+function throttle<A = any, B = any, C = any, D = any>(
+    cb: (a: A, b: B, c: C, d: D) => any,
+    wait: number,
+    immediate?: boolean
+): (a: A, b: B, c: C, d: D) => void;
+function throttle<A = any, B = any, C = any, D = any, E = any>(
+    cb: (a: A, b: B, c: C, d: D, e: E) => any,
+    wait: number,
+    immediate?: boolean
+): (a: A, b: B, c: C, d: D, e: E) => void;
+function throttle<A = any, B = any, C = any, D = any, E = any, F = any, G = any, RType = any>(
+    cb: ((a?: A, b?: B, c?: C, d?: D, e?: E, f?: F, g?: G, ...rest: RType[]) => any),
+    wait: number,
+    immediate?: boolean
+): ((a?: A, b?: B, c?: C, d?: D, e?: E, f?: F, g?: G, ...rest: RType[]) => void);
+
+function throttle<A = any>(
+    cb: ((...args: any[]) => any),
+    wait: number,
+    immediate: boolean = true
+): ((...args: any[]) => void) {
+    let blocked = false;
+    let cbArgs = null;
+
+    return (...args) => {
+        if (!blocked) {
+            cbArgs = args;
+            blocked = true;
+            setTimeout(() => {
+                blocked = false;
+                if (!immediate) return cb(...cbArgs);
+            }, wait);
+            if (immediate) cb(...cbArgs);
+        }
+    };
 }
