a8009ce43ce61acc2e5bd7f8ecf9104fde710122
Andrew Faulkner <andfaulkner@gmail.com>
2018-06-13 22:11:00 -0400

    Clean docs in types-iso

--- src/types-iso.ts
+++ src/types-iso.ts
@@ -27,7 +27,7 @@ export type StrOrVoid = string | void;
 export type StrOrErr = string | Error;
 
 /**
- * Alias to indicate variable injected by a decorator.
+ * Alias to indicate variable injected by a decorator
  */
 export type Injection<T> = T;
 export {Injection as OptionalInjection};
@@ -54,31 +54,31 @@ export {BoolOrError as BooleanOrError};
 
 /*************************************** HTTP REQUEST TYPES ***************************************/
 /**
- * Most commonly used HTTP Request types.
+ * Most commonly used HTTP Request types
  */
 export type CommonHTTPRequestType = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 export {CommonHTTPRequestType as MainHTTPRequestType};
 export {CommonHTTPRequestType as CommonRequestType};
 
 /**
- * All (known) HTTP Request types.
+ * All (known) HTTP Request types
  */
 export type AnyHTTPReqType = CommonHTTPRequestType | 'OPTIONS' | 'TRACE' | 'CONNECT' | 'HEAD';
 export {AnyHTTPReqType as HTTPRequestType};
 
 /***************************************** TYPE HANDLERS ******************************************/
 /**
- *  Returns true if the value is null, undefined, or a string.
- *  @param {StrOrVoid|RealAny} val Value to type check.
- *  @return {boolean} true if val is null, undefined, or a string.
+ *  Returns true if the value is null, undefined, or a string
+ *  @param {StrOrVoid|RealAny} val Value to type check
+ *  @return {boolean} true if val is null, undefined, or a string
  */
 export const isVoidOrString = (val: StrOrVoid | RealAny): val is undefined | null | string =>
     typeof val === 'undefined' || val === null || isString(val);
 
 /**
- * Detect whether given value is a number. (Note: NaN returns false here).
+ * Detect whether given value is a number. (Note: NaN returns false here)
  * @param {any} val Test if val is a number
- * @return {boolean} If given value is a number, return true; otherwise return false.
+ * @return {boolean} If given value is a number, return true; otherwise return false
  */
 export const isNumber = <T extends number | Number = number>(val: RealAny): val is T => {
     if (typeof val === 'undefined' || val == null) return false;
@@ -127,9 +127,9 @@ export const isNumberLike = <T extends number | Number | string | String = numbe
 export {isNumberLike as isNumLike};
 
 /**
- * Returns true if given value is an integer (does not include num-like strings).
- * @param {any} val Value to check type of.
- * @return {boolean} true if given value is integer.
+ * Returns true if given value is an integer (does not include num-like strings)
+ * @param {any} val Value to check type of
+ * @return {boolean} true if given value is integer
  */
 export const isInteger = <T extends number | Number | string | String = number>(
     val: RealAny
@@ -141,9 +141,9 @@ export const isInteger = <T extends number | Number | string | String = number>(
 export {isInteger as isInt};
 
 /**
- * True if val is an integer or a string that can be converted to an integer.
- * @param {any} val - Item to test.
- * @return {boolean} true if tested item is integer-like (or an integer).
+ * True if val is an integer or a string that can be converted to an integer
+ * @param {any} val Item to test
+ * @return {boolean} true if tested item is integer-like (or an integer)
  */
 export const isIntegerLike = <T extends number | Number | string | String = number>(
     val: RealAny
@@ -173,17 +173,17 @@ export const isString = <T extends string | String = string>(val: RealAny): val 
         (Object.prototype.toString.call(val) === '[object String]' ||
             (val.constructor && Object.getPrototypeOf(val.constructor) === String)));
 /**
- * True if val is a string or a number.
- * @param {any} val - Item to test.
- * @return {boolean} true if tested item is a string or a number.
+ * True if val is a string or a number
+ * @param {any} val Item to test
+ * @return {boolean} true if tested item is a string or a number
  */
 export const isStringOrNumber = (val: RealAny): val is number | Number | string | String =>
     isString(val) || isNumberLike(val);
 
 /**
- * Returns true if val is true or false.
- * @param {any} val - Item to test.
- * @return {boolean} true if val is a boolean.
+ * Returns true if val is true or false
+ * @param {any} val Item to test
+ * @return {boolean} true if val is a boolean
  */
 export const isBoolean = <T extends boolean | Boolean = boolean>(val: any | boolean): val is T => {
     if (val === true || val === false) return true;
@@ -200,8 +200,8 @@ export {isBoolean as isBool};
  * Returns true if the given argument is a moment instance, Date instance, or any string, number,
  * or object that moment is able to parse. Excludes negative numbers and strings that parse to
  * negative numbers, and objects with date-irrelevant keys (e.g. { year: 1123, bear: 'grizzly' })
- * @param {any} val - Item to test for Date-like properties
- * @return {boolean} True if item is date-like.
+ * @param {any} val Item to test for Date-like properties
+ * @return {boolean} True if item is date-like
  */
 export const isDateLike = (val: RealAny): boolean => {
     if (val instanceof moment || val instanceof Date) return true;
@@ -223,12 +223,12 @@ export const isDateLike = (val: RealAny): boolean => {
 };
 
 /**
- * True if the given object is an array. Robust and works across multiple JS environments.
- * @param {any} val Check if val is an array.
- * @return {boolean} True if arg 'value' is an Array,
+ * True if given values is an array (Robust, works across multiple JS envs)
+ * @param {any} val Check if val is an array
+ * @return {boolean} True if arg 'value' is an Array
  */
 export const isArray = <T = any>(val: RealAny): val is T[] => {
-    // Works in fully compliant ES5, ES6, ES7, ES8 ES[+] environments (Node, new browsers, etc.)
+    // Works in fully compliant ES5, ES6, ES7, ES8 ES[+] environments (Node, new browsers, etc)
     if (Array.isArray) return Array.isArray(val);
     // Works in browsers without Array.isArray.
     return !!(
@@ -246,10 +246,10 @@ export const isArray = <T = any>(val: RealAny): val is T[] => {
 };
 
 /**
- * True if the given value is any variant of true ('true', 'True', 'TRUE', 'T', 't', or true).
- * @param {any} val Check if this is a variant of true.
- * @param {boolean} include1CharVal return true if given 't' or 'T' when include1CharVal is true.
- * @return {boolean} true if given value is a variant of true, otherwise false.
+ * True if the given value is any variant of true ('true', 'True', 'TRUE', 'T', 't', or true)
+ * @param {any} val Check if this is a variant of true
+ * @param {boolean} include1CharVal return true if given 't' or 'T' when include1CharVal is true
+ * @return {boolean} true if given value is a variant of true, otherwise false
  */
 export const isTrue = <T extends true | string | String = true>(
     val: RealAny,
@@ -260,10 +260,10 @@ export const isTrue = <T extends true | string | String = true>(
         (val.toLowerCase() === 'true' || (include1CharVal && val.toLowerCase() === 't')));
 
 /**
- * True if the given value is any variant of false ('false', 'False', 'FALSE', 'F', 'f', or false).
- * @param {any} val Check if this is a variant of false.
- * @param {boolean} include1CharVal return true if given 'f' or 'F' when include1CharVal is true.
- * @return {boolean} false if given value is a variant of false, otherwise false.
+ * True if the given value is any variant of false ('false', 'False', 'FALSE', 'F', 'f', or false)
+ * @param {any} val Check if this is a variant of false
+ * @param {boolean} include1CharVal return true if given 'f' or 'F' when include1CharVal is true
+ * @return {boolean} false if given value is a variant of false, otherwise false
  */
 export const isFalse = <T extends false | string | String = false>(
     val: RealAny,
@@ -284,8 +284,8 @@ export const isFunction = <T = ((...args: any[]) => any)>(val: RealAny): val is 
 
 // TODO improve singleton design-time behaviour - i.e. proper type hints + Intellisense.
 /**
- * Any class wrapped in this decorator becomes a singleton immediately.
- * Throws if attempt is made to wrap a non-class.
+ * Any class wrapped in this decorator becomes a singleton immediately
+ * Throws if attempt is made to wrap a non-class
  *
  * @example
  *       @singleton
@@ -296,7 +296,7 @@ export const isFunction = <T = ((...args: any[]) => any)>(val: RealAny): val is 
  *           }
  *       }
  *
- *  // It will now only be possible to create one instance of class SomeSingleton.
+ *  // It will now only be possible to create one instance of class SomeSingleton
  *  @param {T} constructor Class to make into a singleton
  */
 export const singleton = <T extends ClassConstructor>(constructor: T) => {
@@ -320,12 +320,12 @@ export const singleton = <T extends ClassConstructor>(constructor: T) => {
 };
 
 /**
- * Convert item to a number (if given item is of a type that can be converted as such).
- * If not, throw an error if this is specified.
+ * Convert item to a number (if given item is of a type that can be converted as such)
+ * If not, throw an error if this is specified
  * @param {StrOrNum} val Value to cast to a number
- * @param {boolean} throwOnFail (OPT) When true, throw if given type isn't a number.
- *                                    When false, return an Error if given type isn't a number.
- * @return {number|Error|never} value converted to number, Error, or nothing if it threw error.
+ * @param {boolean} throwOnFail (OPT) When true, throw if given type isn't a number
+ *                                    When false, return an Error if given type isn't a number
+ * @return {number|Error|never} value converted to number, Error, or nothing if it threw error
  */
 export const castToNum = (val: StrOrNum, throwOnFail = true): number | Error | never => {
     if (isNumber(val)) return val;
@@ -346,19 +346,21 @@ export const castToNum = (val: StrOrNum, throwOnFail = true): number | Error | n
 const bstbErrMsg = 'Must input true, false, t, f, y, n, yes, or no';
 
 /**
- * Convert string representation of a boolean value to a boolean value. Return error if this
- * isn't possible. If something is already a boolean, it simply passes it through.
+ * Convert string representation of a boolean value to a boolean value
+ * Throw error if conversion isn't possible
+ * Passes boolean values through as-is
  * @example converts 'yes' to true, 'f' to false, 'true' to true, true to true, 'n' to false, etc.
  * @param {string|boolean} val Value to convert to string or boolean. Must be 'y', 'n', 't', 'f',
- *                             'yes', no', 'true', or 'false' (case-insensitive); or a boolean.
- * @return {boolean|Error} true if val is y, t, yes, or true. false if it's n, f, no, or false.
- *                         Otherwise throw.
+ *                             'yes', no', 'true', or 'false' (case-insensitive); or a boolean
+ * @return {boolean|Error} true if val is y, t, yes, or true
+ *                         false if it's n, f, no, or false
+ *                         Otherwise throw
  */
 export const boolStringToBool = (
     val: string | boolean,
     strict: boolean = true
 ): boolean | null | never => {
-    // Ensure not void, undefined, or NaN, and that toString doesn't return null.
+    // Ensure not void, undefined, or NaN, and that toString doesn't return null
     if (
         typeof val === 'undefined' ||
         val === null ||
