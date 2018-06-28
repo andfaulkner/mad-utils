70c00148dfaa3b77b9c951388a6b7fbb63134567
Andrew Faulkner <andfaulkner@gmail.com>
2018-06-27 23:02:37 -0400

    Clean up browser module (doc fixes, prettier, spacing)

--- browser.ts
+++ browser.ts
@@ -1,8 +1,41 @@
-// Import shared modules, and re-export them for top-level access.
-import { array, date, decorator, Enum, error, func, json, locale, number, object, url, search, string,
-         types as isoTypes, validation, dataTypes, stream } from './shared';
-export { array, date, decorator, Enum, error, func, json, locale, number, object, url, search,
-         string, validation, stream }
+// Import shared modules, and re-export them for top-level access
+import {
+    array,
+    date,
+    decorator,
+    Enum,
+    error,
+    func,
+    json,
+    locale,
+    number,
+    object,
+    url,
+    search,
+    string,
+    types as isoTypes,
+    validation,
+    dataTypes,
+    stream,
+} from './shared';
+
+export {
+    array,
+    date,
+    decorator,
+    Enum,
+    error,
+    func,
+    json,
+    locale,
+    number,
+    object,
+    url,
+    search,
+    string,
+    validation,
+    stream,
+};
 
 export * from './src/array';
 export * from './src/date';
@@ -20,31 +53,31 @@ export * from './src/stream';
 export * from './src/string';
 export * from './src/validation';
 
-import { isNode } from 'detect-node';
-export { isNode }
+import {isNode} from 'detect-node';
+export {isNode};
 
 // Import DOM module
 import * as dom from './src/browser/dom';
 export * from './src/browser/dom';
-export { dom }
+export {dom};
 
 // Import event module
 import * as event from './src/browser/event';
 export * from './src/browser/event';
-export { event }
+export {event};
 
 // Import location module
 import * as location from './src/browser/location';
 export * from './src/browser/location';
-export { location }
+export {location};
 
 // Import local-store module
 import * as localStore from './src/browser/local-store';
 export * from './src/browser/local-store';
-export { localStore }
-export { localStore as localStorage }
-export { localStore as localStoreUtils }
-export { localStore as localStorageUtils }
+export {localStore};
+export {localStore as localStorage};
+export {localStore as localStoreUtils};
+export {localStore as localStorageUtils};
 
 // Import browser-types (including merged-in types from types-iso)
 import * as browserTypes from './src/browser/types-browser';
@@ -52,10 +85,8 @@ export * from './src/browser/types-browser';
 export * from './src/types-iso';
 export * from './src/types-data-generic';
 
-
-// Build final browser types object by merging isomorphic types with browser-specific types.
+// Build final browser types object by merging isomorphic types with browser-specific types
 export const types = Object.assign(isoTypes, browserTypes, dataTypes);
-
 
 /********************************************* EXPORT *********************************************/
 /**
@@ -75,7 +106,7 @@ export const mUtils = {
     error,
     event,
     func,
-    'function': func,
+    function: func,
     functionUtils: func,
     genericDataTypes: dataTypes,
     isNode,
@@ -101,16 +132,16 @@ export const mUtils = {
     validation,
 };
 
-// Easier to access the 'pseudo-namespaced' mUtils/madUtils module.
+// Easier to access the 'pseudo-namespaced' mUtils/madUtils module
 export const __ = mUtils;
 export const m_ = mUtils;
 export const madUtils = mUtils;
 
 /************************************ COMMON FUNCTION EXPORTS *************************************/
-import { commonShared } from './shared';
+import {commonShared} from './shared';
 
 /**
- * Most common functions from mad-utils used in browser.
+ * Most common functions from mad-utils used in browser
  */
 export const common = Object.assign({}, commonShared, {
     parseUserAgent: dom.parseUserAgent,
