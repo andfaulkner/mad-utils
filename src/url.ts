3feeef5cf1685d23ee54038e42f8888dca01427f
Andrew Faulkner <andfaulkner@gmail.com>
2018-07-09 10:51:15 -0400

    Fix docs in  URL module

--- src/url.ts
+++ src/url.ts
@@ -11,7 +11,7 @@ declare global {
     interface WorkerGlobalScope extends NodeJS.Global {} // Augment Web Worker `self`
 }
 
-// Attach empty location object to global in Node (or anywhere global.location isn't present).
+// Attach empty location object to global in Node (or anywhere global.location isn't present)
 const isNode = require('detect-node');
 if (isNode) global.location = global.location || ({href: '', pathname: '', search: ''} as any);
 
@@ -23,20 +23,24 @@ import {StrOrErr} from './types-iso';
 
 /****************************************** QUERY PARAMS ******************************************/
 /**
- * Turn query params into JS obj (based on splitting on , and =). Return null if no query params. If
- * no param is given, uses the global query params at time of initial page load are used. Careful:
- * this can be unexpected. To play it safe, explicitly pass global.location.search in every time.
- * @param {string} queryParamsString: source to parse for query params. Default: query (?) in URL.
- * @return {Object} Query params as object.
- * @example parseQueryParams('http://example.com/home?hello=everyone&gr=argh')
- *          // => { hello: 'everyone', gr: 'argh' }
+ * Turn query params into JS obj - splits on , & =
+ * Return null if no query params
+ * If no param is given, uses the global query params at time of initial page load
+ *
+ * Careful: this can be unexpected - to play it safe, explicitly pass
+ * global.location.search in every time
+ * @param {string} queryParamsString: source to parse for query params {Default: query in URL}
+ * @return {Object} Query params as object
+ * @example parseQueryParams(`http://example.com/home?hello=everyone&gr=argh`)
+ *          // => {hello: `everyone`, gr: `argh`}
  */
 export const parseQueryParams = <T>(queryParamsStr?: string): T => {
     const queryParamsString = queryParamsStr || global.location.search;
-    // Ensure there are actually query parameters present. Return null otherwise.
-    // Various types of query param strings that actually signify no query params.
+    // Ensure there are actually query parameters present; return null otherwise
+    // Various types of query param strings that actually signify no query params
     if (queryParamsString.match(/((\?)|(\?\=)|(\?\&)|(\?\=\&)|(\?\&\=))$/)) return null;
-    // If no query param symbols are present in a string, it cannot be a query param string.
+
+    // If no query param symbols are present in a string, it cannot be a query param string
     if (
         queryParamsString.split('=').length === 1 &&
         queryParamsString.split('?').length === 1 &&
@@ -44,7 +48,8 @@ export const parseQueryParams = <T>(queryParamsStr?: string): T => {
     ) {
         return null;
     }
-    // Parse the query params if they exist.
+
+    // Parse the query params if they exist
     return queryParamsString
         .replace(/^\?/, '')
         .split('&')
@@ -56,11 +61,14 @@ export const parseQueryParams = <T>(queryParamsStr?: string): T => {
 
 /******************************************** LANGUAGE ********************************************/
 /**
- * Get current language from URL. Assumes lang stored in own path & that 2-letter (/en/) form used.
- * @param {string} urlPath URL to search. Uses global.location.pathname if not provided {OPT}
- * @param {Array<string>} supportedLangs Detectable languages. Default: ['en', 'fr'] {OPT}
- * @param {string} defaultLang Default language, if none detected. Default: 'en' {OPT}
- * @return {string} current language, in 2-letter form. Often either 'en' or 'fr'.
+ * Get current language from URL
+ * Assumes language stored in its own path, and that 2-letter form used - e.g. `/en/`, `/fr/`
+ *
+ * @param {string} urlPath URL to search {Default: global.location.pathname if not provided}
+ * @param {Array<string>} supportedLangs Detectable languages {Default: ['en', 'fr']}
+ * @param {string} defaultLang Default language, if none detected {Default: 'en'}
+ *
+ * @return {string} current language, in 2-letter form (Often either 'en' or 'fr')
  */
 export const getLangFromUrlPathname = (
     urlPath?: string,
@@ -89,13 +97,15 @@ export type UrlPathsLangProps = {
 
 /**
  * Get all paths in the URL before or after the first appearance of /:curLang/
- * If getStrBeforeLang property is given and is true, get the string before the language match.
- * Otherwise get the string after the language match.
+ * If getStrBeforeLang property is given and is true, get the string before the language match
+ * Otherwise get the string after the language match
+ *
  * @example urlPathsAfterLang('/asdf/en/one/two') // => 'one/two'
- * @param {string} [OPTIONAL] url URL to search. If not provided, uses global.location.pathname
- * @param {string} [OPTIONAL] curLang Default language, if none detected. Default: 'en'
- * @param {Array<string>} [OPTIONAL] supportedLangs Detectable languages. Default: ['en', 'fr']
- * @param {boolean} getStrBeforeLang [OPTIONAL] If true, ret pre-match str; else ret post-match str.
+ *
+ * @param {string} url URL to search {Default: global.location.pathname}
+ * @param {string} curLang Default language, if none detected {Default: 'en'}
+ * @param {Array<string>} supportedLangs Detectable languages {Default: ['en', 'fr']}
+ * @param {boolean} getStrBeforeLang If true, return pre-match str; otherwise return post-match str
  */
 export const getUrlPathAroundLang = (
     props: (UrlPathsLangProps & {getStrBeforeLang?: boolean}) | string | null
@@ -129,10 +139,12 @@ export {getUrlPathAroundLang as getPreOrPostLangUrlPaths};
 
 /**
  * Get all paths in the URL following the first appearance of /:curLang/
+ *
  * @example urlPathsAfterLang('/asdf/en/one/two') // => 'one/two'
- * @param {string} [OPTIONAL] url URL to search. If not provided, uses global.location.pathname
- * @param {string} [OPTIONAL] curLang Default language, if none detected. Default: 'en'
- * @param {Array<string>} [OPTIONAL] supportedLangs Detectable languages. Default: ['en', 'fr']
+ *
+ * @param {string} url URL to search {Default: global.location.pathname}
+ * @param {string} curLang Default language, if none detected {Default: 'en'}
+ * @param {Array<string>} supportedLangs Detectable languages {Default: ['en', 'fr']}
  */
 export const getUrlPathAfterLang = (props: UrlPathsLangProps | string | null): StrOrErr => {
     const propsObj = typeof props === 'object' ? props : {};
@@ -146,10 +158,12 @@ export {getUrlPathAfterLang as postLangUrlPaths};
 
 /**
  * Get all paths in the URL prior to the first appearance of /:curLang/
+ *
  * @example urlPathsAfterLang('/asdf/en/one/two') // => '/asdf'
- * @param {string} [OPTIONAL] url URL to search. If not provided, uses global.location.pathname
- * @param {string} [OPTIONAL] curLang Default language, if none detected. Default: 'en'
- * @param {Array<string>} [OPTIONAL] supportedLangs Detectable languages. Default: ['en', 'fr']
+ *
+ * @param {string} url URL to search {Default: global.location.pathname}
+ * @param {string} curLang Default language, if none detected {Default: 'en'}
+ * @param {Array<string>} supportedLangs Detectable languages {Default: ['en', 'fr']}
  */
 export const getUrlPathBeforeLang = (props: UrlPathsLangProps | string | null): StrOrErr => {
     const propsObj = typeof props === 'object' ? props : {};
@@ -162,13 +176,14 @@ export const getUrlPathBeforeLang = (props: UrlPathsLangProps | string | null): 
 export {getUrlPathBeforeLang as preLangUrlPaths};
 
 /**
- * Swaps language code in the given URL to the provided value.
+ * Swaps language code in the given URL to the provided value
+ *
  * @param {string} newLang New language code to swap to. e.g. 'en' OR 'fr'
  * @param {Object|string} props
- *        - @param {string} url {OPT} URL to swap language code in.
- *        - @param {string} curLang {OPT} current lang. If provided, will only swap matching path.
- *        - @param {string[]} supportedLangs {OPT} Allowed languages codes. If given, will only
- *                                                 swap path matching an item in the array
+ *        - @param {string} url URL to swap language code in
+ *        - @param {string} curLang current lang; if provided, will only swap matching path
+ *        - @param {string[]} supportedLangs Allowed language codes (if given, will only
+ *                                           swap path matching an item in the array)
  * @return {string} New URL with language swapped.
  */
 export const getUrlWithLangSwapped = (
@@ -177,9 +192,9 @@ export const getUrlWithLangSwapped = (
 ): string => `${getUrlPathBeforeLang(props)}${newLang}${getUrlPathAfterLang(props)}`;
 
 /**
- * Return copy of the given (or current) URL with the query parameters removed.
- * @param {string} url - [OPTIONAL] url to copy & rm query params from. Defaults to current URL.
- * @return {string} Copy of given (or current) URL sans query params.
+ * Return copy of the given (or current) URL with the query parameters removed
+ * @param {string} url URL to copy & rm query params from {Default: current URL}
+ * @return {string} Copy of given (or current) URL sans query params
  */
 export const urlMinusQueryParams = (url?: string): string => {
     const cleanUrl: string = typeof url === 'string' ? url : global.location.href;
@@ -187,12 +202,15 @@ export const urlMinusQueryParams = (url?: string): string => {
 };
 
 /**
- * Get the last path in the given URL, with query params excluded. No / is prepended to the return
- * val. Returns '' if no paths in url. Sets 'strict mode' to true by default, meaning trailing
- * slashes are not ignored, and if one is present, return value becomes ''.
- * @param {string} href - [OPTIONAL] URL to examine.
- * @param {boolean} strict - [OPTIONAL] If false, ignore trailing slashes. DEFAULT: true.
- * @return {string} last path. No query params. Not prepended by /. '' if trailing / & strict==true
+ * Get the last path in the given URL with query params excluded
+ * No '/' is prepended to the return val
+ * Returns '' if there are no paths in the URL
+ * By default ("strict mode") trailing slashes are not ignored (if present, return becomes '')
+ *
+ * @param {string} href URL to examine
+ * @param {boolean} strict If false, ignore trailing slashes {Default: true}
+ * @return {string} last path with no query params, and not prepended by '/'
+ *                  Returns '' if strict is true and there's a trailing '/' in the URL
  */
 export const lastUrlPath = (url?: string, strict: boolean = true): string => {
     const cleanUrl: string = typeof url === 'string' ? url : global.location.href;
@@ -200,12 +218,12 @@ export const lastUrlPath = (url?: string, strict: boolean = true): string => {
     if (!hrefMinusProtocol.includes('/')) return '';
 
     const hrefMinusQueryParams = urlMinusQueryParams(hrefMinusProtocol);
-    const finalHref = !strict ? chomp(hrefMinusQueryParams, '/') : hrefMinusQueryParams;
+    const finalHref = strict ? hrefMinusQueryParams : chomp(hrefMinusQueryParams, '/');
     return last(finalHref.split('/'));
 };
 
 /**
- * Get query string from the given URL (or the global URL). Excludes "?".
+ * Get query string from the given URL (or the global URL), excluding "?"
  */
 export const urlGetQuery = (url?: string): string => {
     const cleanUrl: string = typeof url === 'string' ? url : global.location.href;
@@ -218,10 +236,11 @@ export {urlGetQuery as urlGetQueryString};
 export {urlGetQuery as urlGetQueryParamString};
 
 /**
- * Return the URL with the protocol string ('http://', 'https://') removed.
- * @param {string} url - to remove protocol string from
- * @return {string} url with protocol string removed
+ * Return the URL with the protocol string ('http://', 'https://') removed
+ *
  * @example urlWithoutProtocol('https://www.exmpl.ca/1/2?k1=v1') // => www.exmpl.ca/1/2?k1=v1
+ * @param {string} url URL to remove protocol string from
+ * @return {string} url with protocol string removed
  */
 export const urlWithoutProtocol = (url?: string): string => {
     const cleanUrl: string = typeof url === 'string' ? url : global.location.href;
@@ -231,8 +250,8 @@ export const urlWithoutProtocol = (url?: string): string => {
 export {urlWithoutProtocol as urlMinusProtocol};
 
 /**
- * Get protocol string from the given URL - either http://, https://, or '' if none given.
- * @param {string} url - to get protocol string from
+ * Get protocol string from the given URL - either http://, https://, or '' if none given
+ * @param {string} url to get protocol string from
  * @return {string} protocol string - either: 'http://', 'https://', or (if none present) ''
  */
 export const urlProtocolString = (url?: string): string => {
@@ -249,8 +268,8 @@ export {urlProtocolString as getProtocolStringFromURL};
 
 /**
  * Get URL minus the last path. e.g. https://localhost:80/a/b => https://localhost:80/a
- * @param {string} url URL to extract from. {DEF: global.location.href} {OPT}
- * @return {string} Given URL minus the last path. If URL had no paths, return base URL.
+ * @param {string} url URL to extract from {Default: global.location.href}
+ * @return {string} Given URL minus the last path, or base URL if URL has no paths
  */
 export const urlMinusLastPath = (url?: string, excludeQuery = true): string => {
     const cleanUrl: string = typeof url === 'string' ? url : global.location.href;
@@ -262,7 +281,7 @@ export const urlMinusLastPath = (url?: string, excludeQuery = true): string => {
     const urlNoQueryLastPathHttp =
         urlParts.length === 1 ? urlParts[0] : without.last(urlParts).join('/');
 
-    // Re-include protocol string; and if excludeQuery == false, also re-include query string.
+    // Re-include protocol string; and if excludeQuery == false, also re-include query string
     return (
         httpStr + (excludeQuery ? urlNoQueryLastPathHttp : `${urlNoQueryLastPathHttp}?${queryStr}`)
     );
@@ -272,9 +291,9 @@ export {urlMinusLastPath as getURLMinusLastPath};
 export {urlMinusLastPath as getUrlMinusLastPath};
 
 /**
- * Get URL minus the last path. e.g. https://localhost:80/a/b => https://localhost:80/a
- * @param {string} url URL to extract from. {DEF: global.location.href} {OPT}
- * @return {string} Given URL minus the last path. If URL had no paths, return base URL.
+ * Get URL minus the last path - e.g. https://localhost:80/a/b => https://localhost:80/a
+ * @param {string} url URL to extract from {Default: global.location.href}
+ * @return {string} Given URL minus the last path,or base URL if URL has no paths
  */
 export const swapLastURLPath = (newPathVal: string, url?: string): string => {
     const cleanUrl: string = typeof url === 'string' ? url : global.location.href;
@@ -283,10 +302,11 @@ export const swapLastURLPath = (newPathVal: string, url?: string): string => {
 };
 
 /**
- * Swap URL path matching given string. Avoids swapping base 'host' value (e.g. www.exmpl.com).
- * @param {string|RegExp} pathMatcher - Value to test for in each of the URL's paths
- * @param {string} newPathVal - Value to swap into the URL
- * @param {string} url - URL to swap path in {DEF: global.location.href} {OPT}
+ * Swap URL path matching given string, avoiding swapping base 'host' value (e.g. www.exmpl.com)
+ *
+ * @param {string|RegExp} pathMatcher Value to test for in each of the URL's paths
+ * @param {string} newPathVal Value to swap into the URL
+ * @param {string} url URL to swap path in {Default: global.location.href}
  * @return {string} URL with the matching path swapped for the given path
  */
 export const swapMatchingURLPaths = (
