d9bad2afd65b849f597ebb5ca7e2db9cf648f46b
Andrew Faulkner <andfaulkner@gmail.com>
2018-07-16 23:31:37 -0400

    Run prettier on test module & fix docs

--- src/node/test.ts
+++ src/node/test.ts
@@ -1,9 +1,9 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 
 /********************************** TEST (MOCHA, CHAI) UTILITIES **********************************/
 /**
  * Expect that testValue is an empty object
- * @param {any} testValue - variable to check for emptiness
+ * @param {any} testValue Value to check for emptiness
  */
 export const expectEmptyObject = (testValue: any) => {
     expect(Object.keys(testValue)).to.be.empty;
@@ -13,23 +13,25 @@ export const expectEmptyObject = (testValue: any) => {
 };
 
 /**
- * Function exists if this runs without throwing. Creates a mocha unit test to ensure this. Use
- * given function name in test output. Display extraMsg after the rest of the test description.
- * @param {Function} func - Function to test
- * @param {string} name? - Name of function {OPTIONAL}
+ * Create mocha unit test to ensure this function exists (success if this runs without throw)
+ * Use given function name in test output
+ * Display extraMsg after the rest of the test description
+ *
+ * @param {Function} func Function to test
+ * @param {string} name? Name of function {OPTIONAL}
  */
 export const expectFuncExists = (func: Function, name?: string, extraMsg?: string) => {
     it(`has function ${name ? name : func.name}${extraMsg ? ' ' + extraMsg : ''}`, function() {
         expect(func).to.exist;
         expect(func).to.be.a('function');
-    })
+    });
 };
 
 /**
- * Object exists and is not empty if this runs without throwing.
+ * Object exists and is not empty if this runs without throwing
  *
- * @param {Object} nonEmptyObj - Object to test.
- * @param {string} name? - Name of function {OPTIONAL}
+ * @param {Object} nonEmptyObj Object to test
+ * @param {string} name? Name of function {OPTIONAL}
  */
 export const expectNonEmptyObjectExists = (nonEmptyObj: any, name: string, extraMsg?: string) => {
     it(`${name} exists and is not empty${extraMsg ? '. ' + extraMsg : ''}`, function() {
@@ -42,4 +44,4 @@ export const expectNonEmptyObjectExists = (nonEmptyObj: any, name: string, extra
     });
 };
 
-export { expectFuncExists as expectFunctionExists }
+export {expectFuncExists as expectFunctionExists};
