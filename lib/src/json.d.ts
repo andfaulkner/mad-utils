0a120c947e69cb85ed65f7188e29505bd58fca96
Andrew Faulkner <andfaulkner@gmail.com>
2018-07-31 23:27:13 -0400

    Cleans json.ts: prettier, fix docs, rm unused vars etc

--- lib/src/json.d.ts
+++ lib/src/json.d.ts
@@ -1,7 +1,7 @@
 /******************************************** EXPORTS *********************************************/
 /**
- * Stringify, while keeping the functions in position by pre-converting them to strings.
- * @param {Object} obj - Object to convert to a JSON string.
- * @return {string} Stringified form of JSON.stringify with functions kept around.
+ * Stringify, while keeping the functions in position by pre-converting them to strings
+ * @param {Object} obj Object to convert to a JSON string
+ * @return {string} Stringified form of JSON.stringify with functions kept around
  */
 export declare const jsonStringifyWFuncs: (obj: Object) => string;
