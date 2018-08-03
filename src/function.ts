62b81ff2694bc84fb0ef465c344e9ffc4224cf96
Andrew Faulkner <andfaulkner@gmail.com>
2018-08-03 05:50:24 -0400

    throttle fn: transfer keys to throttled fn; pseudo-variadic types (up to 5 args)

--- src/function.ts
+++ src/function.ts
@@ -127,11 +127,47 @@ export function condSwitch(
  * @example throttle(() => console.log('Called!'), 1000);
  *          // 10 "clicks" within 1 second will output 'Called!' only once, on initial "click"
  */
-export function throttle<A, CB extends (...fnArgs: A[]) => void>(
+export function throttle<CB extends () => any>(
     cb: CB,
     wait: number,
+    immediate?: boolean
+): () => void;
+
+export function throttle<A1>(
+    cb: (a1: A1) => any,
+    wait: number,
+    immediate?: boolean
+): (a1: A1) => void;
+
+export function throttle<A1, A2>(
+    cb: (a1: A1, a2: A2) => any,
+    wait: number,
+    immediate?: boolean
+): (a1: A1, a2: A2) => void;
+
+export function throttle<A1, A2, A3>(
+    cb: (a1: A1, a2: A2, a3: A3) => any,
+    wait: number,
+    immediate?: boolean
+): (a1: A1, a2: A2, a3: A3) => void;
+
+export function throttle<A1, A2, A3, A4>(
+    cb: (a1: A1, a2: A2, a3: A3, a4: A4) => any,
+    wait: number,
+    immediate?: boolean
+): (a1: A1, a2: A2, a3: A3, a4: A4) => void;
+
+export function throttle<A1, A2, A3, A4, A5>(
+    cb: (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => any,
+    wait: number,
+    immediate?: boolean
+): (a1: A1, a2: A2, a3: A3, a4: A4, a5: A5) => void;
+
+export function throttle<A>(
+    cb: (...fnArgs: A[]) => any,
+    wait: number,
     immediate = true
-): CB {
+): (...fnArgs: A[]) => void {
     let blocked = false;
     const retVal = (...fnArgs: A[]) => {
         if (!blocked) {
@@ -143,7 +179,8 @@ export function throttle<A, CB extends (...fnArgs: A[]) => void>(
             if (immediate) cb(...fnArgs);
         }
     };
-    return retVal as CB;
+    Object.keys(cb).forEach(k => (retVal[k] = cb[k]));
+    return retVal;
 }
 
 /**
