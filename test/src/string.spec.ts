1d1f42c80d56d5e5ce974d98f254c9fb0f3bdbbb
Andrew Faulkner <andfaulkner@gmail.com>
2018-08-15 08:18:47 -0400

    Remove tests for withLeftIndent function

--- test/src/string.spec.ts
+++ test/src/string.spec.ts
@@ -36,7 +36,6 @@ const {
     toSnakeCase,
     toCamelCase,
     deindent,
-    withLeftIndent,
     isWhitespaceChar,
     isAlphanumericChar,
     isOperatorChar,
@@ -575,6 +574,7 @@ describe(`string sub-module`, function() {
                 Sincerely, The Cookie Monster
             `;
 
+            // prettier-ignore
             expect(outStr).to.eql(
                 `Hello,\n` +
                 `    Is it biscotti I'm looking for?\n` +
@@ -594,38 +594,13 @@ describe(`string sub-module`, function() {
 
             `;
 
+            // prettier-ignore
             expect(outStr).to.eql(
                 `Hello,\n` +
                 `    Is it biscotti I'm looking for?\n` +
                 `        Hello?\n` +
                 `Sincerely, The Cookie Monster\n`
             );
-        });
-    });
-
-    describe(`withLeftIndent -- special string template type`, function() {
-        it(`indents single-line string the given number of spaces`, function() {
-            expect(withLeftIndent`${4}asdf`).to.equal('    asdf');
-            expect(withLeftIndent`${1}asdf`).to.equal(' asdf');
-        });
-        it(`can indent 0 spaces`, function() {
-            expect(withLeftIndent`${0}asdf`).to.equal('asdf');
-        });
-        testWithLeftIndentMultilineBasic();
-        it(`Removes preceding whitespace that exists on every line, replaces with given indent val`, function() {
-            expect(withLeftIndent`${4}
-                First line
-                Second line`).to.eql('    First line\n    Second line');
-        });
-        it(`Removes preceding whitespace that exists on every line, & adds no indent if indent val is 0`, function() {
-            expect(withLeftIndent`${0}
-                First line
-                Second line`).to.eql('First line\nSecond line');
-        });
-        it(`Defaults to indent 0`, function() {
-            expect(withLeftIndent`
-                First line
-                Second line`).to.eql('First line\nSecond line');
         });
     });
 
@@ -818,13 +793,9 @@ describe(`string sub-module`, function() {
     describe(`matchFirst`, function() {
         const testStr = 'TEST :: String to search in';
 
-        it(
-            `if given a string to find, returns 1st matching substring (it'll always be the ` +
-                `string being searched for)`,
-            function() {
-                expect(matchFirst(testStr, 'TEST')).to.eql('TEST');
-            }
-        );
+        it(`if given a string to find, returns 1st matching substring (i.e. the str searched for)`, function() {
+            expect(matchFirst(testStr, 'TEST')).to.eql('TEST');
+        });
         it(`returns empty string if no match`, function() {
             expect(matchFirst(testStr, 'okokok')).to.eql('');
             expect(matchFirst(testStr, /^should_not_match_anything/)).to.eql('');
@@ -837,20 +808,20 @@ describe(`string sub-module`, function() {
 
     describe(`deburrFrenchEnglish`, function() {
         it(`Removes accents from words`, function() {
-            expect(deburrFrenchEnglish("tête-à-tête")).to.equal("tete-a-tete");
-            expect(deburrFrenchEnglish("smörgåsbord")).to.equal("smorgasbord");
+            expect(deburrFrenchEnglish('tête-à-tête')).to.equal('tete-a-tete');
+            expect(deburrFrenchEnglish('smörgåsbord')).to.equal('smorgasbord');
             expect(deburrFrenchEnglish("maître d'hôtel")).to.equal("maitre d'hotel");
-            expect(deburrFrenchEnglish("cause célèbre")).to.equal("cause celebre");
-            expect(deburrFrenchEnglish("cañón")).to.equal("canon");
+            expect(deburrFrenchEnglish('cause célèbre')).to.equal('cause celebre');
+            expect(deburrFrenchEnglish('cañón')).to.equal('canon');
         });
         it(`Leaves words as-is if no accents`, function() {
-            expect(deburrFrenchEnglish("hello")).to.equal("hello");
+            expect(deburrFrenchEnglish('hello')).to.equal('hello');
         });
         it(`Returns empty strings as-is`, function() {
-            expect(deburrFrenchEnglish("")).to.equal("");
+            expect(deburrFrenchEnglish('')).to.equal('');
         });
         it(`Returns null as ''`, function() {
-            expect(deburrFrenchEnglish(null)).to.equal("");
+            expect(deburrFrenchEnglish(null)).to.equal('');
         });
     });
 });
@@ -884,27 +855,4 @@ function testMatchFilenameWithExtensionFunction(
             expect(func(nonMatchingFilename4)).to.be.false;
         });
     });
-}
-
-/*********************************** MESSY withLeftIndent TESTS ***********************************/
-function testWithLeftIndentMultilineBasic() {
-    it(`indents each line in multiline string the given number of spaces`, function() {
-        expect(defMultilineWithLeftIndentStr1()).to.equal('    asdf\n    asdf');
-    });
-    it(`does not indent empty lines`, function() {
-        expect(defMultilineWithLeftIndentStr2()).to.equal('        asdf\n\n        asdf');
-    });
-}
-
-function defMultilineWithLeftIndentStr1() {
-    return withLeftIndent`${4}
-asdf
-asdf`;
-}
-
-function defMultilineWithLeftIndentStr2() {
-    return withLeftIndent`${8}
-asdf
-
-asdf`;
 }
