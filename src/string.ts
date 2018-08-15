df2d145fa5725cf470a366ba1c1997c42b2b5d1d
Andrew Faulkner <andfaulkner@gmail.com>
2018-08-15 08:18:36 -0400

    Remove withLeftIndent function

--- src/string.ts
+++ src/string.ts
@@ -8,77 +8,6 @@ export type Chars = string;
 export type Character = string;
 export type Characters = string;
 
-/***************************************** LOCAL HELPERS ******************************************/
-/**
- * Determine what item in a array of strings has the smallest indent
- * Purpose: to remove this amount of indent from all lines, thus preserving nested indentation
- * We can then define the indentation ourselves in a variable
- *
- * It's all about avoiding problems like this with multiline templates:
- *
- *         <body>
- *             <div id="project-wrapper">
- *                 <div id="react-root">
- *         <!-- Vomit -->
- *         <div id="my-inner-component">
- *             <span class="yay-a-span">
- *                 <div>*cries</div>
- *             </span>
- *         </div>
- *                 </div>
- *             </div>
- *         </body>
- */
-const _detectShortestIndentInArray = (lines: string[] | string): number => {
-    // Ensure input is an array
-    const lineArr = (Array.isArray(lines) ? lines : [lines]) as string[];
-
-    // Set flag to exclude last line from comparison if it's an empty whitespace-only line
-    const excludeLast = !!lines[lines.length - 1].match(/^\s+$/);
-
-    return lineArr.reduce((acc: number, line: string, idx: number) => {
-        // Exclude last line from comparison if it's an empty whitespace-only line
-        if (excludeLast && idx === lines.length - 1) return acc;
-
-        // If any line found with no indent, prevent comparisons for the remainder of the loop
-        if (acc === 0) return acc;
-
-        // Match on current line's indentation
-        const match = line.match(/^\s+/m);
-
-        // If there's no match, there's no indent - set to 0
-        if (!match || !match.input || !match[0]) return 0;
-
-        // If indent length is shorter than the prior shortest, return as new shortest length
-        return match[0].length < acc ? match[0].length : acc;
-    }, 120);
-};
-
-/**
- * Ensure left-size indent makes sense
- * It must be an integer, or string that can parse to an integer
- *
- * @param {number|string} leftPadSize Content of required interpolated item
- *                                    placed directly after the start quote
- * @return {never|void} Throw if leftPadSize is invalid
- */
-const _validateWithLeftIndent = (leftPadSize: number | string): never | void => {
-    const nullLeftPadSize = !leftPadSize && leftPadSize !== 0;
-    const leftPadSizeNotNumberType = typeof parseInt(leftPadSize.toString(), 10) !== 'number';
-    const leftPadSizeIsNaN = isNaN(parseInt(leftPadSize.toString(), 10));
-
-    if (nullLeftPadSize || leftPadSizeNotNumberType || leftPadSizeIsNaN) {
-        throw new Error(`
-            withLeftIndent template strings must receive an interpolated value
-            with an integer immediately after the template string starting token.
-                E.g.: withLeftIndent\`\${4}
-                          Some text goes here.
-                          And also here.\n
-                      \`
-            `);
-    }
-};
-
 /*********************************** EXPORTED STRING FUNCTIONS ************************************/
 /**
  * Capitalize the first letter of a string, and convert other letters in the string to lowercase
@@ -260,6 +189,9 @@ export const removeMatchingText = (str: string, matcherToRm: string | RegExp): s
     replaceAll(str, matcherToRm, '');
 
 /************************************** STRING INTERPOLATION **************************************/
+/**
+ * Perform string deindenting for exported deindent function
+ */
 const deindentFormat = (str: string) => {
     let size = -1;
     return (
@@ -305,73 +237,6 @@ export const deindent = (input, ...args: any[]): string | (() => string) => {
             .join(``)
     );
 };
-
-/**
- * TODO MAKE IT WORK WITH INTERPOLATIONS
- * @export withLeftIndent
- *
- * WARNING: DOES NOT ALLOW INTERPOLATIONS
- *
- * Template string type that allows for properly-indented multiline strings
- *
- * Defines a template string type with the following behaviours:
- *     1. Eliminates all left-size indentation on each line;
- *     2. Can take a single interpolation variable to be placed directly after the start caret,
- *        which must contain an integer or string that can be parsed to an integer;
- *     3. Sets the final left-size indentation to equal the value of said interpolation variable
- *        - If interpolation variable is not given, the value defaults to 0
- *
- * Removes as much left-size whitespace as is present in the shortest indent, then adds the
- * requested number of spaces to the indent
- *
- *     @example  Input:
- *                   |                withLeftIndent`${4}
- *                   |                    def hello name do
- *                   |                        puts "Hello #{name}!"
- *                   |                        puts "Also, hello left indent!"
- *                   |                    end
- *                   |                `
- *
- *               Output:
- *                   |        def hello name do
- *                   |            puts "Hello #{name}!"
- *                   |            puts "Also, hello left indent!"
- *                   |        end
- *
- *               Note: ("|" is the left edge of the file)
- *
- * @return {string} Properly indented string
- */
-export function withLeftIndent(strings, leftPadSize = 0, xz?) {
-    _validateWithLeftIndent(leftPadSize);
-
-    // |** 0 **| Detect position of first 'data' string in raw strings array
-    const firstStringPos = typeof strings[1] !== 'undefined' && strings[1] != null ? 1 : 0;
-
-    // |** 1 **| Convert single string with '\n' delimiting lines to an array split on \n
-    const lines: string[] = strings[firstStringPos].split('\n');
-
-    // |** 2 **| Remove 1st element if it's '' (This is the 'pre-initial-variable' string)
-    if (lines[0].length === 0) lines.shift();
-
-    // |** 3 **| Determine which line has the shortest indent.
-    const shortestIndent = _detectShortestIndentInArray(lines);
-
-    // |** 4 **| Create the indentation string to add & whitespace string to split on
-    const leftPadSpaces = ' '.repeat(leftPadSize);
-    const initialIndent = ' '.repeat(shortestIndent);
-
-    // |** 5 **| Cut out the required number of spaces
-    const linesPreppedArr = lines.map((line: string) =>
-        line.replace(new RegExp(`^${initialIndent}`, 'm'), leftPadSpaces)
-    );
-
-    // |** 6 **| Convert array back to string
-    const linesPrepped = linesPreppedArr.join(`\n`);
-
-    // |** 7 **| Remove trailing whitespace in empty lines, and return result
-    return linesPrepped.replace(/^\s+$/gm, '');
-}
 
 /****************************************** REPEAT CHARS ******************************************/
 /**
