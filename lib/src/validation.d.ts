301c439f827cc5c74c01e391d62a2ec099bb4b7f
Andrew Faulkner <andfaulkner@gmail.com>
2018-09-12 03:15:10 -0400

    Transpile

--- lib/src/validation.d.ts
+++ lib/src/validation.d.ts
@@ -1,3 +1,4 @@
+/**************************************** TYPE DEFINITIONS ****************************************/
 export declare type _RegCond = 'min' | 'max' | 'gt' | 'min_length' | 'lt' | 'max_length' | 'match' | 'no_match' | 'len' | 'length' | 'exact_length' | 'length_equals';
 export declare type _NoMatcherCond = 'match_confirmation';
 export declare type _Matcher = RegExp | number | string;
@@ -17,73 +18,65 @@ export declare type IsVStrOpt = {
 };
 /******************************************** EXPORTS *********************************************/
 /**
- * Test that given string meets all of the validation conditions.
- * Condition format: {type: string, matcher?: RegExp|number|string, errMsg: string}
- *     type - accepted values:
- *         min|gt|min_length - specifies smallest allowed length
- *         max|lt|max_length - specifies longest allowed length
- *         len|length|exact_length|length_equals - specifies the exact length input must be
- *         match - string must match the given regular expression
- *         no_match - string must NOT match the given regular expression
- *         match_confirmation - If present, check that testStr matches confirmStr
- *     matcher: Comparison value. e.g. for type 'len', val of matcher = exact required input length
- *     errMsg: Content of 'errMsg' displays if the conditon is not met.
- *     @example for array item in condition { type: 'min', matcher: 4, err: 'a' } :
- *              Check that testStr is a number that's >= 4. Throw err w message 'a' if it's not.
- * @param {Condition[]} conditions - Confirm that string meets all of these conditions.
- * @param {string} testStr - String to check the conditions against
- * @param {string} confirmStr? - [OPTIONAL] String to ensure testStr is equal to
- *                                          (if match_confirmation present)
- * @param {Function} errDisplayCb? - [OPTIONAL] Function for displaying error message.
- *                                     Defaults to alert.
- * @return {boolean} true if all validation conditions are passed. False if not.
- */
-export declare function isValidString({ conditions, testStr, confirmStr, errDisplayCb }: IsVStrOpt): boolean;
-/**
- * Returns true if email address is probably (but not necessarily) correctly formatted.
+ * Returns true if email address is probably (but not definitely) correctly formatted
  *
- * Remember that it's not actually possible to 100% validate an email address by examining or
- * analyzing it. The only way to truly prove an address is valid is to send it an email and
- * see if you get a response. This checker is thus kept deliberately simple, and intended only
- * to block the most obvious errors. It's also been kept maximally permissive.
+ * This checker is thus deliberately simple and maximally permissive, and
+ * intended only to block the most obvious errors
+ * Reason: it's not possible to 100% validate an email address just by analyzing
+ * it - the only way is to send it an email & see if you get a response
  *
- * Use-case: client-side validation to help prevent users from accidentally submitting invalid
- * email addresses. Intended for improving UX, not for determining the data's actual validity.
+ * Use-case: client-side validation to help prevent users from accidentally
+ * submitting invalid email addresses
+ * Intended for improving UX, not for determining the data's actual validity
  *
- * @param {string} email - email address to check for validity.
- * @return {boolean} true if email is probably valid.
+ * @param {string} email Email address to check for validity
+ * @return {boolean} true if email is probably valid
  */
 export declare const isEmailPotentiallyValid: (email: string) => boolean;
 export declare const isEmailValidBasic: (email: string) => boolean;
 /******************************************** HELPERS *********************************************/
 /**
- * Return true if there are no lowercase letters in the give string.
- * @param {string} str - String to check for lowercase characters.
- * @return {boolean} False if string has any lowercase characters.
+ * Return true if there are no lowercase letters in the give string
+ * @param {string} str - String to check for lowercase characters
+ * @return {boolean} False if string has any lowercase characters
  */
 export declare const noLowercase: (str: string) => boolean;
 /**
- * Return true if there are no uppercase letters in the give string.
- * @param {string} str - String to check for uppercase characters.
- * @return {boolean} False if string has any uppercase characters.
+ * Return true if there are no uppercase letters in the give string
+ * @param {string} str - String to check for uppercase characters
+ * @return {boolean} False if string has any uppercase characters
  */
 export declare const noUppercase: (str: string) => boolean;
 /**
- * Return true if there are no numbers in the give string.
- * @return {boolean} False if string has any numbers.
+ * Return true if there are no numbers in the give string
+ * @return {boolean} False if string has any numbers
  */
 export declare const noNumber: (str: string) => boolean;
 /**
- * Return true if there are no special chars in the given string.
+ * Return true if there are no special chars in the given string
  * Special chars: !@#$%^&*()\[\];:'"~`_\-+=|'"\/\\<>
  * @param  {string}  str String to check for special characters
  * @return {boolean}     True if no special chars are present in the given string
  */
 export declare const noSpecialChars: (str: string) => boolean;
+/**
+ * Returns true if given string is a valid Canadian postal code e.g. M9P 8Y1
+ * If allow3Char param is set to true, also accepts the first 1/2 of a postal
+ * code e.g. M9P
+ * @param  {string}  str              check if this string is a valid postal code
+ * @param  {boolean} threeCharAllowed if true, also return true if only 3 letters long (but valid)
+ * @return {boolean}                  true if str is a valid postal code or 1st 1/2 of postal code
+ */
+export declare const validateCanadaPostalCode: (str?: string, allow3Char?: boolean) => boolean;
 /******************************************** REGEXES *********************************************/
 /**
+ * Match Canadian postal codes & partially inputted Canadian postal codes (including ``)
+ * Case-insensitive
+ */
+export declare const canadaPostalCodePartialRegex: RegExp;
+/**
  * Matches all characters found in English amd French, & almost all in other
- * European/Latin-derived languages.
+ * European/Latin-derived languages
  */
 export declare const latinLangCharRegex: RegExp;
 /**
