5233657876e4743395423b3dc9230804e3e41020
Andrew Faulkner <andfaulkner@gmail.com>
2018-09-13 23:33:39 -0400

    Add link to test for matching chars to isAlphabeticChar

--- src/types-iso.ts
+++ src/types-iso.ts
@@ -94,6 +94,7 @@ export const isVoidOrString = (val: StrOrVoid | RealAny): val is undefined | nul
 /**
  * Returns true if [val] is an alphabetic character,
  * Includes diacritics (accented characters) if [handleAccents] is true
+ * See https://regex101.com/r/gY7rO4/265 to view matching characters
  * @param {string} val Value to check
  * @param {boolean} handleAccents If true, return true for diacritics/accented
  *                                letters (Default: true)
