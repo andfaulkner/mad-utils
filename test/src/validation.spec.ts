52cbb5c56e95cefd2453b1ac7a3ff82e9ace2af0
Andrew Faulkner <andfaulkner@gmail.com>
2018-09-20 13:59:40 -0400

    Add basic canadian postal code tests

--- test/src/validation.spec.ts
+++ test/src/validation.spec.ts
@@ -8,6 +8,7 @@ import {m_, validation} from '../../shared';
 import {validation as validationFromNode} from '../../node';
 import {validation as validationFromBrowser} from '../../browser';
 import * as validationModule from '../../src/validation';
+import {validateCanadaPostalCode} from '../../src/validation';
 
 /********************************************* TESTS **********************************************/
 describe(`validation sub-module`, function() {
@@ -16,4 +17,22 @@ describe(`validation sub-module`, function() {
     expectNonEmptyObjectExists(validationModule, 'validation (import all from validation.ts file)');
     expectNonEmptyObjectExists(validationFromNode, 'validation (from Node export)');
     expectNonEmptyObjectExists(validationFromBrowser, 'validation (from Browser export)');
+    describe(`validateCanadaPostalCode`, function() {
+        it(`Returns true given Canadian postal codes in lowercase with no spaces`, function() {
+            expect(validateCanadaPostalCode(`k3k2k2`)).to.equal(true);
+            expect(validateCanadaPostalCode(`m2m1k2`)).to.equal(true);
+        });
+        it(`Returns true given Canadian postal codes in uppercase with no spaces`, function() {
+            expect(validateCanadaPostalCode(`M6N9W2`)).to.equal(true);
+            expect(validateCanadaPostalCode(`O2P4N8`)).to.equal(true);
+        });
+        it(`Returns true given Canadian postal codes in uppercase with spaces`, function() {
+            expect(validateCanadaPostalCode(`L7V 5E2`)).to.equal(true);
+            expect(validateCanadaPostalCode(`O9O 7V6`)).to.equal(true);
+        });
+        it(`Returns true given Canadian postal codes in lowercase with spaces`, function() {
+            expect(validateCanadaPostalCode(`j8t 7r3`)).to.equal(true);
+            expect(validateCanadaPostalCode(`w4q 6a6`)).to.equal(true);
+        });
+    });
 });
