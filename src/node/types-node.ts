b55e79cc9eb4e220ba737da1105718119de8c5b3
Andrew Faulkner <andfaulkner@gmail.com>
2018-10-02 00:36:47 -0400

    Remove colors import from types-node & Colors export type; Reorganize; Add titles; etc

--- src/node/types-node.ts
+++ src/node/types-node.ts
@@ -1,18 +1,23 @@
 /// <reference types="express" />
 
 import {HandleFunction} from 'connect';
-import {colors} from 'mad-logs/lib/node';
 
 /**
- * Not a true import
- * Just imports the type defs
+ * Not a true import - just imports the type defs
  */
 import {Application, Router} from 'express';
 
 /**
+ * Express application types
+ */
+export type ExpressApp = Application | Router;
+
+/******************** MIDDLEWARE TYPES ********************/
+/**
  * Generic signature for Express (slash Connect) middlewares
  */
 export type MWare<T> = (T: T) => HandleFunction;
+
 /**
  * Generic signature for Express (slash Connect) middlewares
  * Alias for MWare type
@@ -23,13 +28,3 @@ export type Middleware<T> = MWare<T>;
  * Function for applying a middleware to a given object
  */
 export type ApplyMiddlewareFn = <T extends Object | object | Function>(app: T) => T;
-
-/**
- * Express application types
- */
-export type ExpressApp = Application | Router;
-
-/**
- * Hack to get the private "Color" type from from the colors module
- */
-export type Color = typeof colors.reset;
