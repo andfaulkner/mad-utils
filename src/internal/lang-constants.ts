17ef291e906c46cdeea58656ccfdd63b371627ee
Andrew Faulkner <andfaulkner@gmail.com>
2018-10-02 01:09:04 -0400

    Clean lang-constants: Run prettier, add docs

--- src/internal/lang-constants.ts
+++ src/internal/lang-constants.ts
@@ -1,8 +1,33 @@
-export const englishVariants = ['en_in', 'en', 'en_ca', 'en_us', 'en_gb', 'en_au', 'en_bz', 'en_cb',
-                                'english', 'en_ie', 'en_jm', 'en_nz', 'en_ph', 'en_za', 'en_tt', 'eng'];
+/**
+ * Array of variants of English, by locale
+ */
+export const englishVariants = [
+    'en_in',
+    'en',
+    'en_ca',
+    'en_us',
+    'en_gb',
+    'en_au',
+    'en_bz',
+    'en_cb',
+    'english',
+    'en_ie',
+    'en_jm',
+    'en_nz',
+    'en_ph',
+    'en_za',
+    'en_tt',
+    'eng',
+];
+
+/**
+ * Array of variants of French, by locale
+ */
 export const frenchVariants = ['french', 'fr', 'fr_fr', 'fr_ca', 'fr_lu', 'fr_ch'];
 
 /**
- * Being from Canada, these are my defaults. You can override them anywhere they're used.
+ * Note: being from Canada, these are my defaults
+ * This may not make sense if you're from somewhere elsewhere
+ * You can override them in any function where they're used
  */
 export const defaultSupportedLangs = ['en', 'fr'];
