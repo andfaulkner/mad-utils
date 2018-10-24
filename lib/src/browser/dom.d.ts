9687f98b58dec557712f0871684f0bbc0567b1b0
Andrew Faulkner <andfaulkner@gmail.com>
2018-10-24 04:11:40 -0400

    Transpile

--- lib/src/browser/dom.d.ts
+++ lib/src/browser/dom.d.ts
@@ -1,63 +1,65 @@
 /// <reference path="../../../node_modules/@types/ua-parser-js/index.d.ts" />
 /// <reference path="../../../node_modules/typescript/lib/lib.dom.d.ts" />
 /// <reference types="ua-parser-js" />
+/**************************************** TYPE DEFINITIONS ****************************************/
 export declare type ParsedUserAgent = IUAParser.IResult & {
     raw: string;
 };
 /******************************************* USER AGENT *******************************************/
 /**
- * Memoized parsed browser user agent.
+ * Memoized parsed browser user agent
  */
-export declare var userAgent: any;
+export declare var userAgentParsed: ParsedUserAgent;
 /**
- * Parse the browser's user agent and return an object containing all user agent properties.
- * @param {string} userAgent - Browser's userAgent string
- * @return {ParsedUserAgent} Data object containing all parsed attributes of user agent string.
- *        { raw:     string
- *          ua:      string
- *          os:      { name: string, version: string }
- *          browser: { name: string, version: string }
- *          engine:  { name: string, version: string }}
+ * Parse browser's user agent & return an object with all user agent properties
+ *
+ * @param {string} userAgent Browser's userAgent string
+ * @return {ParsedUserAgent} Data obj w parsed attributes of user agent string:
+ *                           {raw:     string
+ *                            ua:      string
+ *                            os:      {name: string, version: string}
+ *                            browser: {name: string, version: string}
+ *                            engine:  {name: string, version: string}}
  */
 export declare function parseUserAgent(userAgent?: any): ParsedUserAgent;
 /**
- * Return raw (unparsed) browser user agent string. e.g.
- * "Mozilla/5.0 (Macintosh; Intel Mac OS X 17_14_2) AppleWebKit/530.12 " +
- * "(KHTML, like Gecko) Chrome/51.0.3272.211 Safari/530.12"
+ * Return raw (unparsed) browser user agent string
+ * Example:
+ *     "Mozilla/5.0 (Macintosh; Intel Mac OS X 17_14_2) AppleWebKit/530.12 " +
+ *     "(KHTML, like Gecko) Chrome/51.0.3272.211 Safari/530.12"
  */
-export declare const getUserAgentString: (mWindow?: Window) => any;
+export declare const getUserAgentString: (mWindow?: Window) => string;
 /**
- * @return {string} Name of the current computer's operating system. E.g. "Mac OS" or "Windows NT"
+ * Get name of current computer's operating system e.g. "Mac OS", "Windows NT"
+ * @return {string} Name of current computer's OS
  */
-export declare const osName: () => any;
+export declare const osName: () => string;
 /**
- * @return {string} Name of current OS, in snake_case. E.g. "mac_os" or "windows_nt"
+ * Get name of browser, e.g. "Chrome" or "Firefox"
+ * @return {string} Name of browser
  */
-export declare const osNameSnakeCase: () => string;
+export declare const browserName: () => string;
 /**
-* @return {string} Name of the browser. E.g. "Chrome" (using example above), or "Firefox"
+ * Get name of browser's rendering engine
+ *
+ * Example (Chrome):  browserEngineName(); // => "Webkit"
+ * Example (Firefox): browserEngineName(); // => "Gecko"
+ *
+ * @return {string} Name of browser's rendering engine
  */
-export declare const browserName: () => any;
+export declare const browserEngineName: () => string;
 /**
-* @return {string} Name of browser's rendering engine.
-* @example browserEngineName(); // => "Webkit" (Chrome output. See getUserAgentString example above)
-* @example browserEngineName(); // => "Gecko" (Firefox output);
-*
+ * Get machine's OS semver version number (as a string), e.g. "17.14.2"
+ * @return {string} numeric OS version (in string form)
  */
-export declare const browserEngineName: () => any;
+export declare const osVersion: () => string;
 /**
-* @return {string} OS version. E.g. "17.14.2"
+ * Get browser version number (as a string), e.g. "51.0.3272.211"
+ * @return {string} Browser version number string
  */
-export declare const osVersion: () => any;
+export declare const browserVersion: () => string;
 /**
-* @return {string} Browser version. E.g. "51.0.3272.211"
+ * Get version number string of the browser's rendering engine, e.g. "530.12"
+ * @return {string} Browser rendering engine version number (string)
  */
-export declare const browserVersion: () => any;
-/**
-* @return {string} Version of the browser's rendering engine. E.g. "530.12"
- */
-export declare const browserEngineVersion: () => any;
-/******************************************** HELPERS *********************************************/
-/**
- * Get element by ID. TODO: allow getting by class, tag, etc.
- */
+export declare const browserEngineVersion: () => string;
