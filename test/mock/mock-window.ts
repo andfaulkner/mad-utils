2d68d58d185ec8d66595dfad9f044b37bbe37b7f
Andrew Faulkner <andfaulkner@gmail.com>
2018-10-24 04:16:57 -0400

    Clean mock-window: '->`; Format & expand docs; consolidate exports

--- test/mock/mock-window.ts
+++ test/mock/mock-window.ts
@@ -1,47 +1,44 @@
+/***************************************************************************************************
+ *
+ *       @file ./mock_window.ts
+ *
+ *       Mocks the bare minimum amount of the window object to run unit tests
+ *       on 'browser' modules
+ *
+ */
+
 /// <reference path="../../node_modules/@types/mocha/index.d.ts" />
 /// <reference path="../../node_modules/@types/node/index.d.ts" />
 
-/**************************************************************************************************
-*
-*       @file ./mock_window.ts
-*       
-*       Mocks the bare minimum amount of the window object to run the unit tests
-*
-*/
+import {mockBrowserStorage} from './mock-local-storage';
 
-import { mockBrowserStorage } from './mock-local-storage';
-
-export const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 " +
-                         "(KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36";
+export const userAgent =
+    `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 ` +
+    `(KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36`;
 
 const location = {
-    href:            "http://localhost:8080/auth/en/signup/location",
+    href: `http://localhost:8080/auth/en/signup/location`,
     ancestorOrigins: {},
-    origin:          "http://localhost:8080",
-    protocol:        "http:",
-    host:            "localhost:8080",
-    hostname:        "localhost",
-    port:            "8080",
-    pathname:        "/auth/en/signup/location",
-    search:          "",
-    hash:            ""
+    origin: `http://localhost:8080`,
+    protocol: `http:`,
+    host: `localhost:8080`,
+    hostname: `localhost`,
+    port: `8080`,
+    pathname: `/auth/en/signup/location`,
+    search: ``,
+    hash: ``,
 };
-
 
 /********************************************* EXPORT *********************************************/
 /**
- * @export
- * Pieces of window required to run tests.
+ * Pieces of window required to run tests
  */
-const window = {
-    navigator: { userAgent },
+export const window = {
+    navigator: {userAgent},
     location,
     sessionStorage: mockBrowserStorage(),
     localStorage: mockBrowserStorage(),
 };
 
-
 /*************************** AUTOMATICALLY BIND WINDOW TO GLOBAL OBJECT ***************************/
 (global as any).window = window;
-
-export { window }
