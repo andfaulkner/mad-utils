df654ce3910cff6a1fd48b44660584c13197edbe
Andrew Faulkner <andfaulkner@gmail.com>
2020-06-24 21:38:54 -0400

    Fix typings in stream module

--- src/stream.ts
+++ src/stream.ts
@@ -4,79 +4,63 @@
  * Character input stream.
  * Allows you to iterate through and read a string one character at a time.
  *
- * @return {Object} keys: {
- *                     peek: Show next value without removing it from the stream.
- *                     next:  Show the next value and discards it from the stream.
- *                     isEOF: Returns true if stream is at end.
- *                     throw: Throw input stream errors. }
- *                   }
+ * @return {Object} Keys:
+ *             peek: Show next value without removing it from the stream.
+ *             next:  Show the next value and discards it from the stream.
+ *             isEOL: Returns true if stream is at end of line.
+ *             isEOF: Returns true if stream is at end.
+ *             throw: Throw input stream errors. }
  */
 export class CharInputStream {
-    chr: string                            // Current character
-    pos = 0;                               // Position
-    col = 0; line = 0                      // Coordinates (col & line)
+    /*------------------------------------------ STATE -------------------------------------------*/
+    /** Current character */
+    chr: string;
+    /** Current position */
+    pos = 0;
+    /** Current column (position in current line) */
+    col = 0;
+    /** Current line */
+    line = 0;
 
-    /**
-     * @param {string} Input string to iterate through.
-     */
-    constructor (public input: string) {}
+    /** @param {string} Input string to iterate through. */
+    constructor(public input: string) {}
 
+    /*-------------------------- ITERATION METHODS ---------------------------*/
+    /** Return the next value without removing it from the stream. */
+    public peek = (): string => this.input.charAt(this.pos);
 
-    //
-    // ITERATION METHODS
-    //
-
-    /**
-     * @returns the next value without removing it from the stream.
-     */
-    public peek = (): string => this.input.charAt(this.pos)
-
-    /**
-     * @returns the next value and discards it from the stream
-     */
+    /** Return the next value and discards it from the stream. */
     public next = (): string => {
-        this.chr = this.input.charAt(this.pos++);      // Get chr, store in object, shift pos
-        if (this.isEOL()) this.eol(); else this.col++; // Shift row & col based on whether at eol
+        // Get chr, store in object, shift pos
+        this.chr = this.input.charAt(this.pos++);
+
+        // Shift row & col based on whether at eol
+        if (this.isEOL()) this.eol();
+        else this.col++;
+
         return this.chr;
-    }
+    };
 
-
-    //
-    // ERROR HANDLING
-    //
-
-    /**
-     * Throw an error with the given message, also displaying the current stream position.
-     */
+    /*---------------------------- ERROR HANDLING ----------------------------*/
+    /** Throw error with given message, also displaying the current stream position. */
     public throw = (msg: string): never => {
         throw new Error(`${msg} (Position ${this.pos}: line ${this.line}, col ${this.col})`);
-    }
+    };
 
+    /*---------------------------- CHARACTER INFO ----------------------------*/
+    /** Return true if at end of file (no more vals in stream), otherwise false */
+    public isEOF = (): boolean => this.peek() === '';
 
-    //
-    // CHARACTER INFO
-    //
+    /** Return true if at end of line (i.e. if given char is '\n') */
+    public isEOL = (ch?: string): boolean => (ch || this.chr) === '\n';
 
-    /**
-     * @returns true if at end of file (no more vals in stream), otherwise false
-     */
-    public isEOF = (): boolean => this.peek() === ''
+    /* Return true if current or given char is a whitespace char (including newline) */
+    public isWhitespace = (ch?: string): boolean => '\t\ns '.indexOf(ch || this.chr) >= 0;
 
-    /**
-     * @return true if at end of line (i.e. if given character is '\n')
-     */
-    public isEOL = (ch?: string): boolean => (ch || this.chr) === '\n'
-
-    /**
-     * @return true if current or given char is a whitespace character (including newline)
-     */
-    public isWhitespace = (ch?: string): boolean => '\t\n\s '.indexOf(ch || this.chr) >= 0;
-
-
-    //
-    // PRIVATE HELPERS
-    //
-
-    // Handle end of line (shift column & line)
-    private eol = (): void => { this.col = 0; this.line++; }
+    /*--------------------------- PRIVATE HELPERS ----------------------------*/
+    /** Handle end of line (shift column & line) */
+    private eol = (): void => {
+        this.col = 0;
+        this.line++;
+    };
 }
