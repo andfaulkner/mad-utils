fde63ed81f8b760cf8a6b2cd16e895bc0fb667df
Andrew Faulkner <andfaulkner@gmail.com>
2022-12-23 06:54:17 -0500

    Rm silly & outdated gender & sex typings and functions

--- src/types-data-generic.ts
+++ src/types-data-generic.ts
@@ -1,95 +1,7 @@
+/*--------------------------------------- PROJECT MODULES ----------------------------------------*/
 import {deepFreeze} from './object';
 
-/***************************************** SEX AND GENDER *****************************************/
-//
-// SEX
-//
-
-export const biologicalSexes = deepFreeze({
-    male: 'male' as 'male',
-    female: 'female' as 'female',
-});
-export {biologicalSexes as commonBiologicalSexes};
-export {biologicalSexes as sexes};
-export {biologicalSexes as commonSexes};
-
-export const biologicalSexesWithBoth = deepFreeze(
-    Object.assign({}, biologicalSexes, {
-        both: 'both' as 'both',
-    })
-);
-export {biologicalSexesWithBoth as commonBiologicalSexesWithBoth};
-export {biologicalSexesWithBoth as sexesWithBoth};
-export {biologicalSexesWithBoth as commonSexesWithBoth};
-export {biologicalSexesWithBoth as sexWithBoth};
-
-export const biologicalSexesWithOther = deepFreeze(
-    Object.assign({}, biologicalSexes, {
-        other: 'other' as 'other',
-    })
-);
-export {biologicalSexesWithOther as sexesWithOther};
-export {biologicalSexesWithOther as commonSexesWithOther};
-export {biologicalSexesWithOther as commonBiologicalSexesWithOther};
-
-// Sex types
-export type BiologicalSex = keyof typeof biologicalSexes;
-export {BiologicalSex as Sex};
-export {BiologicalSex as CommonSex};
-export {BiologicalSex as BinarySex};
-
-export type BiologicalSexWithOther = keyof typeof biologicalSexesWithOther;
-export {BiologicalSexWithOther as SexWithOther};
-export {BiologicalSexWithOther as CommonSexWithOther};
-
-export type BiologicalSexWithBoth = keyof typeof biologicalSexesWithBoth;
-export {BiologicalSexWithBoth as SexWithBoth};
-export {BiologicalSexWithBoth as CommonSexWithBoth};
-export {BiologicalSexWithBoth as VaccineSex}; // < How sexes are defined in vaccine schedules.
-export {BiologicalSexWithBoth as VaccineSexes};
-
-//
-// GENDER
-//
-
-// Types, carried over from sex.
-export {BiologicalSexWithOther as Gender};
-export {BiologicalSexWithOther as GenderCommon};
-
-export {BiologicalSexWithBoth as GenderWithBoth};
-export {BiologicalSexWithBoth as GenderCommonWithBoth};
-
-// Objects, carried over form sex.
-export {biologicalSexesWithOther as gender};
-export {biologicalSexesWithOther as genderCommon};
-
-export {biologicalSexesWithBoth as genderWithBoth};
-export {biologicalSexesWithBoth as genderCommonWithBoth};
-
-// Types, carried over from sex.
-
-/**
- * Extended gender list.
- */
-export const genderFull = deepFreeze(
-    Object.assign({}, biologicalSexesWithOther, {
-        transMale: 'trans male' as 'trans male',
-        transFemale: 'trans female' as 'trans female',
-        genderqueer: 'genderqueer' as 'genderqueer',
-        genderfluid: 'genderfluid' as 'genderfluid',
-        intersex: 'intersex' as 'intersex',
-        agender: 'agender' as 'agender',
-        bigender: 'bigender' as 'bigender',
-        nonBinary: 'nonBinary' as 'nonBinary',
-        none: 'none' as 'none',
-    })
-);
-
-export type GenderFull = keyof typeof genderFull;
-export {GenderFull as GenderExtensive};
-export {GenderFull as GenderExtended};
-
-/***************************************** PROVINCE TYPES *****************************************/
+/*---------------------------------------- PROVINCE TYPES ----------------------------------------*/
 export type CANProvince =
     | 'Ontario'
     | 'Quebec'
@@ -111,7 +23,7 @@ export type CANProvinceOrNone = CANProvince | 'none';
 export {CANProvince as CanadianProvince};
 export {CANProvinceOrNone as CanadianProvinceOrNone};
 
-/************************************ EXPORT PROVINCE OBJECTS *************************************/
+/*----------------------------------- EXPORT PROVINCE OBJECTS ------------------------------------*/
 export type ProvinceOrNoneObject = {[key: string]: CANProvinceOrNone};
 
 /**
--- test/src/types-data-generic.spec.ts
+++ test/src/types-data-generic.spec.ts
@@ -1,62 +1,29 @@
 /// <reference path="../../node_modules/@types/mocha/index.d.ts" />
 
+/*------------------------------------- THIRD-PARTY MODULES --------------------------------------*/
 import {expect} from 'chai';
+
+/*--------------------------------------- PROJECT MODULES ----------------------------------------*/
 import {expectNonEmptyObjectExists} from '../../src/node/test';
 
-/******************************** IMPORT ERROR MODULE FOR TESTING *********************************/
-import {
-    canadianProvinces,
-    canadianProvincesOrNone,
-    sexes,
-    sexesWithOther,
-    gender,
-    genderFull,
-} from '../../shared';
+/*------------------------------- IMPORT ERROR MODULE FOR TESTING --------------------------------*/
+import {canadianProvinces, canadianProvincesOrNone} from '../../shared';
 
-/********************************************* TESTS **********************************************/
-describe(`Generic data tests`, function() {
-    describe(`Canadian provinces`, function() {
+/*-------------------------------------------- TESTS ---------------------------------------------*/
+describe(`Generic data tests`, function () {
+    describe(`Canadian provinces`, function () {
         expectNonEmptyObjectExists(canadianProvinces, 'Canadian provinces object exists');
         expectNonEmptyObjectExists(
             canadianProvincesOrNone,
             'Canadian provinces object with none included exists'
         );
 
-        it(`canadianProvinces is deep frozen`, function() {
+        it(`canadianProvinces is deep frozen`, function () {
             expect(canadianProvinces).to.be.frozen;
         });
 
-        it(`canadianProvincesOrNone is deep frozen`, function() {
+        it(`canadianProvincesOrNone is deep frozen`, function () {
             expect(canadianProvincesOrNone).to.be.frozen;
-        });
-    });
-
-    describe(`Sex/Gender`, function() {
-        expectNonEmptyObjectExists(sexes, 'Basic sexes object exists');
-        expectNonEmptyObjectExists(sexesWithOther, 'Basic sexes object w "other" included exists');
-        expectNonEmptyObjectExists(
-            gender,
-            'Basic gender object (identical to sexesWithOther) exists'
-        );
-        expectNonEmptyObjectExists(
-            genderFull,
-            'Extensive gender object (with piles of "experimental" & less common genders) exists'
-        );
-
-        it(`sexes is deep frozen`, function() {
-            expect(sexes).to.be.frozen;
-        });
-
-        it(`sexesWithOther is deep frozen`, function() {
-            expect(sexesWithOther).to.be.frozen;
-        });
-
-        it(`gender is deep frozen`, function() {
-            expect(gender).to.be.frozen;
-        });
-
-        it(`genderFull is deep frozen`, function() {
-            expect(genderFull).to.be.frozen;
         });
     });
 });
